4f6788208a304d6c739be52c53097bdd3dad52ae
thePULKITsood <59098007+thePULKITsood@users.noreply.github.com>
2021-06-24 03:19:45 +0530

    Time: 14 ms (73.48%), Space: 39.3 MB (33.75%) - LeetHub

--- all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.java
+++ all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.java
@@ -7,68 +7,73 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+
+ // store the parent in a hashmap and then just perform a bfs search according to level and then it will become easy 
+// this is basically a graph problem but as we can not go ackwards so we need ot make a mapping of parents to children so that we can go back and do a bfs -- so we create a hashmap 
+// node - left right and parent -- for parent we will use hashmap  and a visited set is also required 
+        
+
 class Solution {
+    
+    Map<TreeNode ,TreeNode> map = new HashMap();
+    List<Integer> res = new ArrayList();
+    
     public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
+       
+        if (root == null || k < 0) return res;
+         buildMap(root, null);
         
+
+        Set <TreeNode> visited = new HashSet();
+        visited.add(null);
         
-//           here we have to print k levels down and also to the top and every direction 
-//        step 1 -get the node to root path of the  target then 
+        LinkedList<TreeNode> q = new LinkedList();
+        q.addLast(target);
         
-//         step 2 run print k levels down on the nodes in node to root path here what we do is 
-//          for each of the nodes we decrease the value of k and print k levels down 
+        int level = 0;
         
-//         here we have to be careful with as we must not print all nodes which are at some distance k - i from the nodes in the path 
-        // as they are not k distance away from the target so they must be successfully avoided 
-        
-//          to block we pass a blocker that blocks that call 
-    
-        
-//         function 1 get root to node path 
-        
-        ArrayList<TreeNode> path = new ArrayList();
-        findpath(root,target,path);
-        
-//         now call a loop for the node to root path and call print down on it 
-        List<Integer>  ans = new ArrayList<Integer> ();
-        TreeNode blockNode =null;
-        
-        for (int i  = 0  ; i< path.size() ; i++){
-            printkdown (path.get(i),k-i,blockNode,ans);
-            blockNode = path.get(i);
-        }
-        return ans ;
-    }
-    
-    public void printkdown(TreeNode root , int k , TreeNode blockNode , List<Integer> ans ){
-//         in this there can be several cases 
-        if (k<0 || root == null || root == blockNode  ) return ;
-        if (k==0 )
-        {
-            ans. add(root.val);
-            return ;
-        }
-        printkdown(root.left , k-1 ,blockNode,ans);
-        
-        printkdown(root.right , k-1 ,blockNode,ans);
-        
-        
-    }
-    public boolean findpath(TreeNode root, TreeNode target,ArrayList<TreeNode> path)
-    {
-        if (root == null) return false;
-        
-        if (root == target ){
-            path.add(root);
-            return true;
+        while(q.size() > 0){
+            
+            if (level == k ){
+                while (q.size()>0){
+                    TreeNode temp = q.removeFirst();
+                    res.add(temp.val);
+                }
+                return res;
+            }
+            
+            int size = q.size();
+            while (size-- > 0){
+                
+                TreeNode temp = q.removeFirst();
+                visited.add(temp);
+                
+//                 addits 3 things 
+                
+                TreeNode parent = map.get(temp);
+                TreeNode left = temp.left;
+                TreeNode right = temp.right;
+                
+                if (! visited.contains(parent))q.addLast(parent);
+                if (! visited.contains(left))q.addLast(left);
+                if (! visited.contains(right))q.addLast(right);
+                
+                visited.add(parent);
+                visited.add(left);
+                visited.add(right);
+                
+            }
+            level++;
         }
         
-        boolean res = findpath(root.left, target,path) || findpath(root.right, target,path);
-        if (res){
-            path.add(root);
-          
-        }
         return res;
-//             or works here as it checks both the conditions and our function runs for both if the first one is false and only for first one if it is true 
-        
     }
+    void buildMap(TreeNode node , TreeNode parent){
+        if(node == null)return;
+        
+        map.put(node,parent);
+        buildMap(node.left,node);
+        buildMap(node.right,node);
+    }
+    
 }
