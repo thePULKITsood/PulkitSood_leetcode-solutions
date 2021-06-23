54b269e09d16f144e36568cf134951fea23ce040
thePULKITsood <59098007+thePULKITsood@users.noreply.github.com>
2021-06-23 22:27:13 +0530

    Time: 33 ms (55.70%), Space: 48.1 MB (81.99%) - LeetHub

---
+++ implement-trie-prefix-tree/implement-trie-prefix-tree.java
@@ -0,0 +1,87 @@
+class Trie {
+
+    private class Node {
+        Node[]children;
+        boolean isEnd;
+        
+        
+        Node (){
+            children = new Node [26];
+        }
+    }
+    final private Node root;
+    
+    /** Initialize your data structure here. */
+  
+    public Trie() {
+        root = new Node();
+        
+    }
+    
+    /** Inserts a word into the trie. */
+    
+    
+    public void insert(String word) {
+        
+        Node curr = root ;
+        
+        for (int i = 0 ;i < word.length() ; i++){
+            char ch = word.charAt(i);
+            
+            if (curr.children [ ch-'a'] == null ){
+//                 means this char is not present 
+//                 so insert it there 
+                curr.children [ch-'a'] = new Node ();
+                
+            }
+            
+//             now if it was not there i have inserted it and if it was there i have not run that statement and i am going to move my curr to the next child - either the newly inserted on e or the old one 
+            curr = curr.children[ch-'a'];
+        }
+//         when this loop ends i will mark the  curr.isEnd = true;
+        curr.isEnd = true;
+        
+    }
+    
+    /** Returns if the word is in the trie. */
+    public boolean search(String word) {
+        
+        Node curr = root;
+        for (int i = 0 ; i < word.length() ; i++){
+            char ch = word.charAt(i);
+            if (curr.children[ch-'a'] == null ){
+                return false ;
+            }
+//             matlab wo null nahi hai so move forward
+            curr = curr.children[ch-'a'];
+            
+        }
+        return curr.isEnd;
+        
+    }
+    
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    public boolean startsWith(String prefix) {
+           
+        Node curr = root;
+        for (int i = 0 ; i < prefix.length() ; i++){
+            char ch = prefix.charAt(i);
+            if (curr.children[ch-'a'] == null ){
+                return false ;
+            }
+//             matlab wo null nahi hai so move forward
+            curr = curr.children[ch-'a'];
+            
+        }
+        return true;
+       
+    }
+}
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * boolean param_2 = obj.search(word);
+ * boolean param_3 = obj.startsWith(prefix);
+ */
