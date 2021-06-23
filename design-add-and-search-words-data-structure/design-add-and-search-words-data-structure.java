a951bfeb3baa5809c5d3f152d22bffc9730acd36
thePULKITsood <59098007+thePULKITsood@users.noreply.github.com>
2021-06-23 23:32:46 +0530

    Time: 36 ms (95.04%), Space: 50.1 MB (35.70%) - LeetHub

--- design-add-and-search-words-data-structure/design-add-and-search-words-data-structure.java
+++ design-add-and-search-words-data-structure/design-add-and-search-words-data-structure.java
@@ -1,10 +1,11 @@
 class WordDictionary {
 
     private class Node {
-        Node[]children;
+        Node [] children;
         boolean isEnd ;
         
-        Node (){
+        Node    ()
+        {
             children = new Node[26];
         }
     }
@@ -21,8 +22,10 @@ class WordDictionary {
         
         for (int i=0 ; i< word.length ();i++){
             char ch = word.charAt(i);
+            
             if (curr.children[ch-'a'] == null)
                 curr.children[ch-'a'] = new Node ();
+            
             curr = curr.children[ch-'a'];
             
         }
@@ -31,9 +34,10 @@ class WordDictionary {
     
     public boolean search(String word) {
         
-        // now if the ch == '.' i need to loop through all the possibilities and do a dfs so it  must become a helper func 
+// now if the ch == '.' i need to loop through all the possibilities and do a dfs so it  must become a helper func 
+        
        return  help(word, this.root ,0);
-      
+//       the main trick here was how to write the helper function such that the index is managed and i dont have to split the word 
     }
     private boolean help (String word, Node root ,int index){
         Node curr = root;
@@ -42,15 +46,24 @@ class WordDictionary {
         
         char ch =word.charAt(index); 
         
-        if (ch !='.'){
-            if (curr.children[ch-'a'] == null ) return false ;
-            else return help (word,curr.children[ch-'a'] , index + 1 );
-        }
-        else 
+        if (ch !='.')
         {
-            for (Node n : curr.children){
-                if (n != null ){
-                    if (help(word,n,index + 1)) return true;
+            if (curr.children[ch-'a'] == null ) return false ;
+            
+            else 
+            {
+                curr = curr.children[ch-'a'];
+                return help (word,curr , index + 1 );
+            }
+        }
+        
+        else  // meaning it is . 
+        {
+            for (Node n : curr.children)
+            {
+                if (n != null )
+                {
+                    if (help(word , n , index + 1)) return true;
                 }
             }
         }
