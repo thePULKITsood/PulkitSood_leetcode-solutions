1007249ec4104326a66cc538ebb2fc7814102781
thePULKITsood <59098007+thePULKITsood@users.noreply.github.com>
2021-06-27 00:16:00 +0530

    Time: 9 ms (29.81%), Space: 165.6 MB (8.93%) - LeetHub

--- longest-zigzag-path-in-a-binary-tree/longest-zigzag-path-in-a-binary-tree.java
+++ longest-zigzag-path-in-a-binary-tree/longest-zigzag-path-in-a-binary-tree.java
@@ -14,32 +14,15 @@
  * }
  */
 class Solution {
-    class pair {
-        int forw = -1 ; 
-        int back = -1 ;
-        int maxbelowme = 0 ;
-        pair(){
-            
-        };
-        pair (int a , int b , int c ){
-            forw = a;
-            back = b ; 
-            maxbelowme = c;
-        }
-    }
+    
     public int longestZigZag(TreeNode root) {
-        pair ans= longestZigZag_(root);
-        return ans.maxbelowme;
+        return dfs(root)[2];
     }
-    public pair longestZigZag_(TreeNode root){
-        if (root == null) return new pair();
-        pair left = longestZigZag_( root.left);
-        
-        pair right = longestZigZag_(root.right);
-        
-        int incme =Math.max (left.back , right.forw ) + 1 ;
-        
-        int maxbelowme = Math.max(Math.max(left.maxbelowme , right.maxbelowme) , incme)
-;        return new pair (left.back + 1 , right.forw + 1 , maxbelowme);
+
+    private int[] dfs(TreeNode root) {
+        if (root == null) return new int[]{-1, -1, -1};
+        int[] left = dfs(root.left), right = dfs(root.right);
+        int res = Math.max(Math.max(left[1], right[0]) + 1, Math.max(left[2], right[2]));
+        return new int[]{left[1] + 1, right[0] + 1, res};
     }
 }
