cfc4767355b13908eb770672beb42b4fde154711
thePULKITsood <59098007+thePULKITsood@users.noreply.github.com>
2021-06-27 03:39:12 +0530

    Time: 0 ms (100.00%), Space: 38.8 MB (42.73%) - LeetHub

--- binary-tree-cameras/binary-tree-cameras.java
+++ binary-tree-cameras/binary-tree-cameras.java
@@ -14,67 +14,28 @@
  * }
  */
 class Solution {
-
-//              either have camera       
-//           /
-//     node / 
-//          |--  or i have no camera  
-//                                     \
-//                                      \ 1. i am covered    - my child has camera 
-//                                       \
-//                                        \ 2. i am not covered - either give me a camera or cover me 
-//                                         |                                                       
-                 
-//                  not talking about the parent as i am going from bottom to top 
-    
-    
- // -- greedy  solution 
-    
-//     if you need a camera  = -1 
-//         if you have a camers = 0 
-//             if you are covered = 1 
-    int camera = 0;
+    private int NOT_MONITORED = 0;
+    private int MONITORED_NOCAM = 1;
+    private int MONITORED_WITHCAM = 2;
+    private int cameras = 0;
+	
     public int minCameraCover(TreeNode root) {
-        
-        int ans = minCameraCover_(root);
-        if (ans == -1) return camera + 1;
-        return camera;
-        
-    }
-   int  minCameraCover_ (TreeNode root ){
-       
-        if (root == null ) return 1;
-       
-        int left = minCameraCover_(root.left);
-        int right = minCameraCover_(root.right);
-        
-        
-        if (left == -1 || right == -1 ){
-            camera ++;
-            
-            // i will have to place a camera as my child needs it and i will pass i have camera 
-            
-            return 0;
-        }
-        
-        if (left == 0 || right == 0 ){
-            // if my left has camera or my right has camera 
-//             i am covered 
-            return 1;
-        }
-        if (left == 1  && right == 1 ){
-            
-            // my left is covered and my right is covered
-            // then i must not put a camera on me i must return -1 so that my parent can know i need a camera 
-            
-                return -1 ;
-        }
-        return -1 ;
-        
+        if (root == null) return 0;
+        int top = dfs(root);
+        return cameras + (top == NOT_MONITORED ? 1 : 0);
     }
     
-    
-    
-    
-    
+    private int dfs(TreeNode root) {
+        if (root == null) return MONITORED_NOCAM;
+        int left = dfs(root.left);
+        int right = dfs(root.right);
+        if (left == MONITORED_NOCAM && right == MONITORED_NOCAM) {
+            return NOT_MONITORED;
+        } else if (left == NOT_MONITORED || right == NOT_MONITORED) {
+            cameras++;
+            return MONITORED_WITHCAM;
+        } else {
+            return MONITORED_NOCAM;
+        }
+    }
 }
