8a9d32dba1f6965fc9b1616f901ed395dfc25fb9
thePULKITsood <59098007+thePULKITsood@users.noreply.github.com>
2021-06-30 10:52:14 +0530

    Time: 1 ms (100.00%), Space: 38.6 MB (73.59%) - LeetHub

---
+++ add-binary/add-binary.java
@@ -0,0 +1,29 @@
+class Solution {
+    public String addBinary(String a, String b) {
+     
+        
+//          in first we can use the addittion operation 
+        
+//         in other we cannot use th eaddittion operation so we must do it with bit manipulation 
+        
+        StringBuilder sb = new StringBuilder ();
+        int i = a.length()-1  , j = b.length()-1;
+        int carry = 0;
+        
+        while (i>=0 || j>= 0 ){
+            int sum = carry ;
+            if (i>=0) 
+                sum+=(a.charAt(i) - '0');
+            
+            if (j>=0) 
+                sum+=(b.charAt(j) - '0');
+            
+            sb.append(sum%2);
+            carry = sum/2;
+            i--;j--;
+                
+        }
+        if (carry!=0)sb.append(carry);
+        return sb.reverse().toString();
+    }
+}
