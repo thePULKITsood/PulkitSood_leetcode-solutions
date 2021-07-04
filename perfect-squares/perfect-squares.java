d350f3238c32b9b6555bf55a6ccf683eb703f04d
thePULKITsood <59098007+thePULKITsood@users.noreply.github.com>
2021-07-04 21:31:54 +0530

    Time: 32 ms (70.46%), Space: 38.2 MB (62.64%) - LeetHub

--- perfect-squares/perfect-squares.java
+++ perfect-squares/perfect-squares.java
@@ -1,17 +1,22 @@
 class Solution {
+    
     public int numSquares(int n) {
-        int dp[] = new int [n+1];
-        Arrays.fill(dp,Integer.MAX_VALUE);
         
+        int dp[] = new int [n+1];
+        
+        Arrays.fill(dp,Integer.MAX_VALUE);
         
         dp[0]=0;
         //base case 
         
-        for (int i=1;i<=n;i++){
+        for (int i=1 ; i<=n ; i++){
+            
             //for finding each value of the dp array 
             //now to find the value of one cell 
+            
             for (int j=1 ; j*j<=i ; j++)
-                dp[i]=Math.min(dp[i],dp[i-j*j] + 1 );
+                
+                dp[i]= Math.min (dp[i] , dp[ i - j*j ]  + 1 );
             
         }
         return dp[n];
