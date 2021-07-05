d8f8e3fabab85043cf5570b395936f963a5c3af8
thePULKITsood <59098007+thePULKITsood@users.noreply.github.com>
2021-07-05 15:44:01 +0530

    Time: 1 ms (70.55%), Space: 37.3 MB (61.33%) - LeetHub

---
+++ jewels-and-stones/jewels-and-stones.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        HashSet <Character> set = new HashSet ();
+        for (char c : jewels.toCharArray()){
+            set.add(c);
+        }
+        int count =0;
+        for (char c : stones.toCharArray()){
+            if (set.contains(c))
+                count ++;
+        }
+        return count;
+    }
+}
