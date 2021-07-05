6c443d6c2f7e4d1d63a487bdc8ed39bf17581dca
thePULKITsood <59098007+thePULKITsood@users.noreply.github.com>
2021-07-05 14:59:43 +0530

    Time: 3 ms (99.61%), Space: 39.4 MB (23.69%) - LeetHub

--- edit-distance/edit-distance.java
+++ edit-distance/edit-distance.java
@@ -1,30 +1,42 @@
 class Solution {
-    public int minDistance(String word1, String word2) {
-          int m = word1.length();
-        int n = word2.length();
-        
-        int [][]dp = new int [m+1][n+1];
-        
-        for(int i = 0; i <= m; i++)
-            dp[i][0] = i;
-        for(int i = 1; i <= n; i++)
-            dp[0][i] = i;
+    public int minDistance(String s1, String s2) {
+        int m =  s1.length();
+        int n = s2.length();
+        int [][] memo =new int  [m+1][n+1];
+        // Arrays.fill(memo,-1);
+        return help ( s1,s2,m,n,memo);
         
         
-        
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
-                
-                if(word1.charAt(i -1 ) == word2.charAt(j-1 )) {
-                    dp[i][j] = dp[i - 1][j - 1];
-                }
-                else 
-                {
-                    dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i][j - 1], dp[i - 1][j])) + 1;
-                }
-            }
-        }
-        
-return dp[m][n];
-        }
     }
+      int  help(String s1, String s2,int i ,int j ,int [][]memo )
+      {
+        if (memo[i][j] != 0)
+            return memo[i][j];
+          
+        if (i==0) 
+            return memo[i][j] = j ;
+          
+        else if (j==0) 
+            return memo[i][j] = i;
+        
+        if (s1.charAt(i - 1 ) == s2.charAt(j - 1 ))
+        {
+            return (memo[i][j]= help (s1,s2, i-1 , j-1 ,memo));
+        }
+          
+        else
+        {
+            
+            int  insert  = help (s1,s2,i , j-1 ,memo);
+            
+            int  delete = help  (s1,s2,i-1,j,memo);
+            
+            int  replace = help (s1,s2,i-1,j-1,memo);
+            
+        
+            return (memo[i][j]= 1 + Math.min(insert , Math.min(delete,replace)));
+        }
+          
+        
+    }
+}
