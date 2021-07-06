2c3202a472215c5b05b2d668b3a5343de59233f0
thePULKITsood <59098007+thePULKITsood@users.noreply.github.com>
2021-07-07 02:46:01 +0530

    Time: 2 ms (49.05%), Space: 38.7 MB (60.71%) - LeetHub

--- validate-stack-sequences/validate-stack-sequences.java
+++ validate-stack-sequences/validate-stack-sequences.java
@@ -1,22 +1,23 @@
 class Solution {
     public boolean validateStackSequences(int[] pushed, int[] popped) {
-        int n = pushed.length ;
+       
         int j = 0 ;
-        
         Stack <Integer> st = new Stack ();
         
         for (int x : pushed ){
-            st.push(x);
+            st.push(x); 
+            // ik elemnt push kar ke sirf ik hi ni pop karana we have to pop unilt they are similar
             
-            // ik elemnt push kar ke sirf ik hi ni pop karana we have to pop unilt they are similar 
-            while (!st.isEmpty() &&  j< n && st.peek() == popped[j])
+            while (!st.isEmpty() &&   st.peek() == popped[j])
             
             {
                 j++;
                 st.pop();            
             }
         }
-        return j==n;
+        return st.empty();
         
     }
 }
+
+// algoritm - push --> then pop till we get the pop sequence same then keep pushing and popping 
