90ed92c6a1e6ab4c7db3da44161f4f56e1ebd345
thePULKITsood <59098007+thePULKITsood@users.noreply.github.com>
2021-07-09 02:08:15 +0530

    Time: 15 ms (78.42%), Space: 40 MB (45.81%) - LeetHub

---
+++ minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.java
@@ -0,0 +1,54 @@
+class Solution {
+    public String minRemoveToMakeValid(String s) {
+        
+   
+        int b = 0 ; 
+        StringBuilder sb = new StringBuilder ();
+        
+        for (char c : s.toCharArray()){
+            if (c =='('){
+                b++;
+                sb.append(c);
+            }
+            else if ( c==')'){
+                if (b!= 0){
+                sb.append(c);
+                b--;
+                }
+            }
+            else {
+                sb.append(c);
+                
+            }
+            //now we have the right ones sorted 
+        }
+//         now we need to do the same thing on the string builder in reverse 
+        
+        sb=sb.reverse();
+        String newst= new String(sb);
+        sb = new StringBuilder ();
+        b=0;
+        
+        for (char c : newst.toCharArray()){
+            if (c ==')'){
+                b++;
+                sb.append(c);
+            }
+            else if (c=='(') {
+                if (b!= 0){
+                sb.append(c);
+                b--;
+                }
+            }
+            else {
+                sb.append(c);
+                
+            }
+            //now we have the right ones sorted 
+        }
+        return sb.reverse().toString();
+        
+        
+        
+    }
+}
