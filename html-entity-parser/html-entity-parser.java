a240b8d725ea3695a0d06dd7c5b8cb00a72f1e9f
thePULKITsood <59098007+thePULKITsood@users.noreply.github.com>
2021-07-09 00:00:28 +0530

    Time: 74 ms (18.93%), Space: 77.5 MB (7.41%) - LeetHub

--- html-entity-parser/html-entity-parser.java
+++ html-entity-parser/html-entity-parser.java
@@ -1,71 +1,20 @@
 class Solution {
     public String entityParser(String text) {
         
-        HashMap<String, String> map = new HashMap<>();
-        map.put("&quot;","\"");
+        Map<String, String> map = new HashMap<>();
+        
         map.put("&apos;", "'");
-        map.put("&amp;", "&");
+        map.put("&quot;", "\"");
         map.put("&gt;", ">");
         map.put("&lt;", "<");
         map.put("&frasl;", "/");
         
-         StringBuilder ans = new StringBuilder(), HTML = new StringBuilder();
+        for (String key: map.keySet()) {
+            
+            text = text.replaceAll(key, map.get(key));
+        }
         
-         boolean and = false;
-         char ch[] = text.toCharArray();
-        
-            for (char c : ch){
-                
-               if(c == '&') 
-                   
-              {
-                   if (and)//means an and in an and 
-                   {
-                       ans.append(HTML);
-                       HTML = new StringBuilder();
-                        HTML.append("&");
-                       
-                   }
-                   else
-                   {
-                HTML.append("&");
-                and = true;
-                   }
-              }
-                
-                else 
-                {
-                if (c ==';' )
-                {
-                    HTML.append(";");
-                    String s = HTML.toString();
-                    
-                    if(map.containsKey(s))
-                        ans.append(map.get(s));
-                    else
-                        ans.append(s);
-                    
-                    HTML= new StringBuilder();
-                    and = false;
-                }
-                    else {
-                         if(and)
-                     
-                             HTML.append(c);
-                     else
-                        ans.append(c);
-                    }
-                    
-                }
-                
-            }
-        return ans.append(HTML).toString();
-        
-        
-        
-        
+        text = text.replaceAll("&amp;", "&");
+        return text;
     }
 }
-//  1. break the string into a string array 
-//  2. look for & as open and ; as close and change the values 
-//  3. at the end change the values of the & as we do not change it in the  first time 
