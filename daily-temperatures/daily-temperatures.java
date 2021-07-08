f7d878f59501052b78280660816d969f5536e768
thePULKITsood <59098007+thePULKITsood@users.noreply.github.com>
2021-07-08 22:47:26 +0530

    Time: 87 ms (17.61%), Space: 122.3 MB (5.32%) - LeetHub

---
+++ daily-temperatures/daily-temperatures.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int[] dailyTemperatures(int[] t) {
+        int n = t.length;
+        
+        Stack <Integer> st = new Stack ();
+        
+        int ans [] = new int [n];
+        
+        st.push( n-1 );
+        // ans[n-1] = 0;
+        
+        for (int i = n-2 ; i>=  0 ; i--)
+        {
+            while (st.size () >0  && t[i] >=t[st.peek()])
+                st.pop();
+            
+            if (st.size ()==0)
+                ans[i]=0;
+            
+            else 
+                ans[i]=st.peek()-i;
+            
+            st.push(i);
+        }
+        
+        return ans;
+    }
+}
