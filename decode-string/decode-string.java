b393347c6c7204eb81a0e635cc0c62fc19a9f11f
thePULKITsood <59098007+thePULKITsood@users.noreply.github.com>
2021-07-09 01:14:17 +0530

    Time: 8 ms (5.21%), Space: 38.8 MB (13.99%) - LeetHub

--- decode-string/decode-string.java
+++ decode-string/decode-string.java
@@ -1,72 +1,60 @@
 class Solution {
     public String decodeString(String s) {
-        
-        // will be done using a pointer  and a string to duplicate 
-        
-        Stack <Character> st = new Stack();
-        StringBuilder sb = new StringBuilder();
-        
-        for (char ch : s.toCharArray())
-        {
-            if (Character.isDigit(ch))
-               st.push(ch);
-           
-            else if (st.size()==0)
-                  sb.append(ch);
+          Stack<Integer> countStack = new Stack<>();
+        Stack<String> resStack = new Stack<>();
+     
+         String res = "";
+        int idx = 0;
+        while (idx < s.length()) {
             
-           else {
-               
-                st.push(ch);
+            if (Character.isDigit(s.charAt(idx))) {
                 
-                if (ch == ']')
+                int count = 0;
+                
+                while (Character.isDigit(s.charAt(idx)))
                 {
-                    st.pop();
-                    StringBuilder part  = new StringBuilder();
-                    
-                    while (st.size() >0  && st.peek()!='[')
-                        part.append(st.pop());
-                    
-                    part = part.reverse();
-                        
-                    st.pop();
-                    // popping the [
- 
-                    
-                    ArrayList<Integer> num = new ArrayList();
-                    
-                    while (st.size()>0  && Character.isDigit( st.peek() ) )
-                         num.add(st.pop()-'0');
-                    
-                    
-                    int mul= 0 ;
-                    
-                    for (int i = num.size() -1 ; i>=0 ; i--){
-                    mul= mul*10 +  num.get(i);    
-                    }
-                    
-                    
-                    StringBuilder toadd  = new StringBuilder();
-                   while (mul -- > 0)
-                     toadd.append(part);    
-                
-                    if (st.size()==0)
-                        sb.append(toadd);
-                    else{         
-                        String str = toadd.toString();
-                        for (char c : str.toCharArray() ){
-                            st.push(c);
-                        }
-                    }
+                    count = 10 * count + (s.charAt(idx) - '0');
+                    idx++;
                 }
-              
+                countStack.push(count);
             }
             
-        }
-        
-        for (char ch : st){
-            sb.append(ch);
-        }
-        return sb.toString();
-        
+            else if (s.charAt(idx) == '[') {
+                
+                resStack.push(res);
+                res = "";
+                idx++;
+            }
+            
+             else if (s.charAt(idx) == ']') 
+             {
+                StringBuilder temp = new StringBuilder (resStack.pop());
+                 
+                int repeatTimes = countStack.pop();
+                 
+                for (int i = 0; i < repeatTimes; i++)
+                {
+                    temp.append(res);
+                }
+                 
+                res = temp.toString();
+                idx++;
+            }
+              
+            else {
+                res += s.charAt(idx++);
+            }
+
     }
+        return res;
+    }
+    
 }
+
+// But the trick here is that there can be nested encoded strings like k[string k[string]]. For example, string s =3[a2[c]]. In such cases, we must decode the innermost string and continue in an outward direction until the entire string is decoded.
+
+// The pattern begins with a number k, followed by opening braces [, followed by string. Post that, there could be one of the following cases :
+
+// There is another nested pattern k[string k[string]]
+// There is a closing bracket k[string]
+// Since we have to start decoding the innermost pattern first, continue iterating over the string s, pushing each character to the stack until it is not a closing bracket ]. Once we encounter the closing bracket ], we must start decoding the pattern.
