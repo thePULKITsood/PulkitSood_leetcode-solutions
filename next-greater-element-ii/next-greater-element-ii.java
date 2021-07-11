77f774e22d52eb2734088ee1fd71475122e84a17
thePULKITsood <59098007+thePULKITsood@users.noreply.github.com>
2021-07-11 20:27:02 +0530

    Time: 18 ms (21.89%), Space: 52.8 MB (5.85%) - LeetHub

--- next-greater-element-ii/next-greater-element-ii.java
+++ next-greater-element-ii/next-greater-element-ii.java
@@ -1,44 +1,32 @@
 class Solution {
-    public int[] nextGreaterElements(int[] nums) {
-     
-        // loop 1 - n-2 to 0    - only tow operations pop and poush  not making answer 
-            
-        // loop 2 - n-1 to 0 - the actual   pop   - ans -- push 
-            
-        int n = nums.length;
-        Stack <Integer> st = new Stack ();
+    public int[] nextGreaterElements(int[] num) {
         
-        int ans[] = new int [n];
-        for (int i = n-2 ; i >=0 ; i--){
-//             pop push  - no ans 
+        // we have to go from left to right 
+        
+        // the answer is made by the one who makes the  pop 
+        // i will just make two loops and if the st.peek()== i then its ngr = -1 
+           
+        
+        int n  = num.length;
+ 
+        int ans [] = new int [n];
+        Arrays.fill(ans,-1);
+        
+        Stack<Integer> st = new Stack();
+        
+        for (int i = 0 ;i < 2*n ; i++ ){
+            int idx = i%n;
             
-            while (! st.isEmpty() && st.peek() < nums[i])
-                st.pop();
+            while (!st.isEmpty() && num[st.peek()]< num[idx]) 
+                ans[st.pop()] = num[idx];
             
-            st.push(nums[i]);
-        }
-        
-        
-        
-        
-        
-             for (int i = n-1 ; i >=0 ; i--){
-//             pop   ans  push  
+//             if (num[st.peek()] == num[idx])
+//                 ans[]
+            st.push(idx);
             
-            while (! st.isEmpty() && st.peek() <= nums[i])
-                st.pop();
-            
-                 
-                 ans[i] = st.size()== 0 ? -1 : st.peek();
-                 
-                st.push(nums[i]);
         }
         return ans;
+            
+        
     }
 }
-
-// what we did we processed all the elements from last to front  without making the answer then we did the same thing 
-// we made our answer from the right to the left 
-
-// int the first pass we dont make the answer as the answer will be wrong thus we directyly  make the answer in
-// the next pass 
