61d94ecd71c0a4b96ff1e39741afa0700e5eba3c
thePULKITsood <59098007+thePULKITsood@users.noreply.github.com>
2021-07-11 21:15:20 +0530

    Time: 44 ms (14.26%), Space: 52.2 MB (5.48%) - LeetHub

--- minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.java
+++ minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.java
@@ -1,57 +1,35 @@
 class Solution {
     public String minRemoveToMakeValid(String s) {
+        Stack<Integer> st  = new Stack ();
+//         we will make an array or map of indexes where we will store the things which are not to be added in the final string 
         
-   
-        int b = 0 ; 
-        StringBuilder sb = new StringBuilder ();
-        
-        for (char c : s.toCharArray()){
-            if (c =='('){
-                b++;
-                sb.append(c);
-            }
-            else if ( c==')'){
-                
-                if (b!= 0){
-                sb.append(c);
-                b--;
-                }
+        for (int i = 0; i< s.length() ; i++){
+            
+            char ch = s.charAt(i);
+            if (ch=='('){
+                st.push(i);
             }
             
-            else 
-            {
-                sb.append(c);
-                
-            }
-            //now we have the right ones sorted 
-        }
-//         now we need to do the same thing on the string builder in reverse 
-        
-        sb=sb.reverse();
-        String newst= new String(sb);
-        sb = new StringBuilder ();
-        b=0;
-        
-        for (char c : newst.toCharArray()){
-            if (c ==')'){
-                b++;
-                sb.append(c);
-            }
-            else if (c=='(') {
-                if (b!= 0){
-                sb.append(c);
-                b--;
+            else if (ch==')'){
+                if (st.isEmpty())
+                    st.push(i);
+                else if (s.charAt(st.peek()) =='(')
+                {
+                    st.pop();
                 }
+                else st.push(i);
             }
-            else {
-                sb.append(c);
-                
-            }
-            //now we have the right ones sorted 
+            
         }
-        return sb.reverse().toString();
-        
-        
+        Set<Integer> indexesToRemove = new HashSet<>();
+         while (!st.isEmpty()) indexesToRemove.add(st.pop());
+                StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            if (!indexesToRemove.contains(i)) {
+                sb.append(s.charAt(i));
+            }
+        }
+        return sb.toString();
         
     }
 }
