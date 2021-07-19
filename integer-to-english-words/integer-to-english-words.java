60fcf9563e8f184e91f60414dc9c3fa10479c885
thePULKITsood <59098007+thePULKITsood@users.noreply.github.com>
2021-07-19 14:26:59 +0530

    Time: 14 ms (21.36%), Space: 39.3 MB (34.43%) - LeetHub

--- integer-to-english-words/integer-to-english-words.java
+++ integer-to-english-words/integer-to-english-words.java
@@ -1,30 +1,80 @@
 class Solution {
-     private final String[] belowTen = new String[] {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"};
-     private final String[] belowTwenty = new String[] {"Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
-     private final String[] belowHundred = new String[] {"", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
-    
+    private final String[] t = {"", "Thousand", "Million", "Billion"};
+     Map<Integer,String> map;
     public String numberToWords(int num) {
-       if (num == 0) return "Zero";
+            if (num == 0) return "Zero";
+        
+        map = new HashMap();
+        map.put(1,"One");    
+      map.put(2, "Two");
+      map.put(3, "Three");
+      map.put(4, "Four");
+      map.put(5, "Five");
+      map.put(6, "Six");
+      map.put(7, "Seven");
+      map.put(8, "Eight");
+      map.put(9, "Nine");
+              
+        map.put(10,"Ten");
+      map.put(11, "Eleven");
+      map.put(12, "Twelve");
+      map.put(13, "Thirteen");
+      map.put(14, "Fourteen");
+      map.put(15, "Fifteen");
+      map.put(16, "Sixteen");
+      map.put(17, "Seventeen");
+      map.put(18, "Eighteen");
+      map.put(19, "Nineteen");
+              
+       map.put(20,"Twenty");
+       map.put(30,"Thirty");
+       map.put(40,"Forty");
+       map.put(50,"Fifty");
+       map.put(60,"Sixty");
+       map.put(70,"Seventy");
+       map.put(80,"Eighty");
+       map.put(90,"Ninety");
+        
     
-         return helper(num); 
+    String words="";
+    int i = 0 ; 
+    
+    while (num>0)
+    {
+        int todo = num%1000;
+    
+        if(todo!=0){
+            words = helper(todo) + t[i] +" " +words;
+        }
+        i++;
+        num=num/1000;
+        
     }
-    private String helper(int num) {
-        String result = new String();
+    return words.trim();
+              
+    }
+    String helper (int num)
+    {
+        if (num==0)
+            return "";
         
-        if (num < 10) result = belowTen[num];
+        else if (num<20)
+            return map.get(num) +" ";
         
-        else if (num < 20) result = belowTwenty[num -10];
+        else if (num<100)
+            return map.get(num/10 * 10 ) +" " + helper (num%10);
         
-        else if (num < 100) result = belowHundred[num/10] + " " + helper(num % 10);
-        
-        else if (num < 1000) result = helper(num/100) + " Hundred " +  helper(num % 100);
-
-        else if (num < 1000000) result = helper(num/1000) + " Thousand " +  helper(num % 1000);
-        
-        else if (num < 1000000000) result = helper(num/1000000) + " Million " +  helper(num % 1000000);
-        
-        else result = helper(num/1000000000) + " Billion " + helper(num % 1000000000);
-        
-        return result.trim();
+        else 
+            return map.get(num/100) + " Hundred " + helper(num % 100);
     }
 }
+
+
+
+
+
+
+
+
+
+
