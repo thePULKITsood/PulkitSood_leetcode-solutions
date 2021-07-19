c498fbb82d0e94a09c0f8e3d4a2d523525e1e216
thePULKITsood <59098007+thePULKITsood@users.noreply.github.com>
2021-07-19 17:49:14 +0530

    Time: 2 ms (98.34%), Space: 38.8 MB (85.88%) - LeetHub

---
+++ valid-palindrome/valid-palindrome.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+//         convert the  string into correct string 
+//         then palindrome check is easy 
+        
+       int left = 0;
+        int right = s.length()-1;
+        while (left<right){
+            while (left<right && !Character.isLetterOrDigit(s.charAt(left)))
+                left++;
+            
+            while (left<right && !Character.isLetterOrDigit(s.charAt(right)))
+                right--;
+            
+            if(Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right)) )
+                return false;
+            
+            left ++;
+            right --;
+        }
+        return true;
+        
+    }
+}
