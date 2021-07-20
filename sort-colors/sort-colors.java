3f700a0442686ce749fcc39119925c8d1e847620
thePULKITsood <59098007+thePULKITsood@users.noreply.github.com>
2021-07-20 10:36:28 +0530

    Time: 0 ms (100.00%), Space: 37.4 MB (70.68%) - LeetHub

---
+++ sort-colors/sort-colors.java
@@ -0,0 +1,37 @@
+class Solution {
+    public void sortColors(int[] nums) {
+//         we will keep pointers i j and k 
+        int n = nums.length;
+        
+        int i = 0;
+        int j = 0 ;
+        int k = n-1;
+        
+        //i must be at the last 1 
+        
+        while (j<=k){
+            
+            if(nums[j]==1)
+                j++;
+            
+            else if (nums[j]==0){
+                //swap i and j 
+                // then i ++ j++
+                // if(i==j)continue;
+                int temp = nums[j];
+                nums[j] = nums[i];
+                nums[i]=temp;
+                
+                i++;
+                j++;
+            }
+            else if (nums[j]==2){
+                nums[j]=nums[k];
+                nums[k]=2;
+                k--;
+            }
+        }
+        
+        
+    }
+}
