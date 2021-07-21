4f256b106895d9cc59fe325af82a7cbb28ecf822
thePULKITsood <59098007+thePULKITsood@users.noreply.github.com>
2021-07-21 17:12:14 +0530

    Time: 4 ms (84.15%), Space: 38.7 MB (95.83%) - LeetHub

---
+++ permutation-in-string/permutation-in-string.java
@@ -0,0 +1,31 @@
+public class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        if (s1.length() > s2.length())
+            return false;
+        
+        int[] s1map = new int[26];
+        int[] s2map = new int[26];
+        
+        for (int i = 0; i < s1.length(); i++) {
+            s1map[s1.charAt(i) - 'a']++;
+            s2map[s2.charAt(i) - 'a']++;
+        }
+        
+        for (int i = s1.length() ; i < s2.length() ; i++) {
+            
+            if (matches(s1map, s2map))
+                return true;
+            
+            s2map[s2.charAt(i) - 'a']++;
+            s2map[s2.charAt(i-s1.length()) - 'a']--;
+        }
+        return matches(s1map, s2map);
+    }
+    public boolean matches(int[] s1map, int[] s2map) {
+        for (int i = 0; i < 26; i++) {
+            if (s1map[i] != s2map[i])
+                return false;
+        }
+        return true;
+    }
+}
