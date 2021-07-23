cab22d693103e979ef4af5d2e320d9bc11dbd6b9
thePULKITsood <59098007+thePULKITsood@users.noreply.github.com>
2021-07-23 14:31:22 +0530

    Time: 9 ms (60.22%), Space: 42.9 MB (63.21%) - LeetHub

---
+++ distinct-subsequences/distinct-subsequences.java
@@ -0,0 +1,29 @@
+class Solution {
+    int n , m ;
+    Integer memo[][];
+    public int numDistinct(String s, String t) {
+        n = s.length();
+        m = t.length ();
+        memo = new Integer [n][m];
+        
+        return solve (s,t,0,0);
+        // return memo[n-1][m-1];
+    
+    }
+    int solve (String s, String t, int i , int j){
+           if(j==m) return 1 ;
+            if(i==n)return 0;
+           
+        
+        if (memo[i][j]!=null)
+            return memo[i][j];
+        
+        if(s.charAt(i)==t.charAt(j))
+            memo[i][j] =  solve(s,t,i+1,j+1) + solve (s,t,i+1 , j);
+        
+        else 
+            memo[i][j]= solve(s,t,i+1,j);
+        
+        return memo[i][j];
+    }
+}
