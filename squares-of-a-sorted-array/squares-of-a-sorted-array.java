3b7dc76a24bcf1822f29ac859a4c56ed2f7a3acd
pulkit sood <59098007+thePULKITsood@users.noreply.github.com>
2021-07-29 03:08:04 +0530

    Time: 1 ms (100.00%), Space: 41.2 MB (25.82%) - LeetHub

---
+++ squares-of-a-sorted-array/squares-of-a-sorted-array.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        int l = 0 ;
+        int r = nums.length - 1;
+        int ans[] = new int [nums.length];
+        int i = nums.length - 1;
+        
+       for (  ; i>=0 ;i--)
+        {
+            if(Math.abs(nums[l]) > Math.abs(nums[r])){
+                
+                ans[i] = nums[l] * nums[l];
+                l++;
+            }
+            else {
+                ans[i] = nums[r] * nums[r];
+                r--;
+                
+            }
+            
+        }
+        return ans;
+    }
+}
