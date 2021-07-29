7fa9b85a5b1fe8538989c9430784799a7000182e
pulkit sood <59098007+thePULKITsood@users.noreply.github.com>
2021-07-29 11:38:46 +0530

    Time: 0 ms (100.00%), Space: 38.7 MB (34.75%) - LeetHub

---
+++ search-a-2d-matrix/search-a-2d-matrix.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+        
+//         binary search 
+        
+        int l = 0 ; 
+        int r = n*m -1 ;
+        
+        while (l<=r){
+            int mid = (l+r) /2 ;
+            int row = mid/m;
+            int col = mid%m;
+            
+            int ele = matrix [row][col];
+            if( target == ele)
+                return true;
+            
+            else if (target> ele)
+                l=mid+1;
+            
+            else 
+                r=mid- 1; 
+            
+                
+        }
+        return false;
+    }
+}
