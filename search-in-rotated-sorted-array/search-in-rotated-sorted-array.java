b8b6e61f15de76b050e8d5f9efa96b9fc4324924
pulkit sood <59098007+thePULKITsood@users.noreply.github.com>
2021-08-04 23:58:49 -0400

    Time: 0 ms (100.00%), Space: 38.5 MB (33.17%) - LeetHub

---
+++ search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int search(int[] arr, int target) {
+        int low = 0;
+        int high = arr.length-1;
+        
+        while (low<=high){
+            int mid = low+(high-low)/2;
+            if(arr[mid]==target)
+                return mid;
+            
+            //if low to mid sorted 
+            //the check if will i get my ans in between the low and mid 
+            
+            //if yes continue if not hten make low = mid+1;
+            
+            if(arr[low] <= arr[mid])//low to mid sorted 
+            {
+                if(target>=arr[low]&& target<arr[mid])
+                    high = mid-1;
+                
+                else //as the part is soted an the target is not here seo move the left 
+                    low = mid+1;
+            }
+            else // low ot mid is not sorted means mid to right is soted
+            {
+                if(target>arr[mid] && target<=arr[high])
+                    low = mid+1;
+                
+                else 
+                    high = mid-1;
+            }
+            
+            
+            //else mid to high sorted 
+                
+        }
+        return -1;
+    }
+}
