f2830092c18d0715a25f7316dbf1da924674a498
pulkit sood <59098007+thePULKITsood@users.noreply.github.com>
2021-08-17 04:54:55 +0530

    Time: 25 ms (14.63%), Space: 53.4 MB (93.16%) - LeetHub

--- continuous-subarray-sum/continuous-subarray-sum.java
+++ continuous-subarray-sum/continuous-subarray-sum.java
@@ -1,29 +1,33 @@
 class Solution {
     public boolean checkSubarraySum(int[] nums, int k) {
-//         set has a flaw as i can not store the index it creates a large number of probles 
-//         so i will use map and store the index we see the index of the sum 
-//         if it has already a index then we are getting it for the second time 
+        HashMap<Integer, List<Integer>>map = new HashMap();
+        map.put(0,new ArrayList());
+        map.get(0).add(-1);
         
-        Map<Integer, Integer> map = new HashMap();
         
-        map.put(0,-1);
+        int sum =0;
+        int rem =0;
         
-        int sum = 0;
-        
-        for (int i = 0 ;i< nums.length  ; i++)
+        for (int i =0 ;i<nums.length ; i++)
         {
-            sum += nums[i];
+            sum +=nums[i];
+            rem = sum%k;
             
-            int mod = k==0 ? sum : sum%k ;
+            if(rem<0)
+                rem+=k;
             
-            int prev = map.getOrDefault(mod,-2);
-            
-            if(prev != -2){
-                if(i-prev > 1 )  // this makes sure the length of the substring is > 1 
-                    return true;
+            if(map.containsKey(rem))
+            {  for (int val : map.get(rem))
+                if(i-val>=2)
+                return true;
             }
-            else 
-                map.put(mod,i);
+        
+            else if(! map.containsKey(rem))
+                map.put(rem,new ArrayList());
+            
+            map.get(rem).add(i);
+        
+            
         }
         return false;
     }
