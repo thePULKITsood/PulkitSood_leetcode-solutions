df930a3ebb50bd283d458609d92c67eb513581a8
pulkit sood <59098007+thePULKITsood@users.noreply.github.com>
2021-08-24 08:05:30 +0530

    Time: 35 ms (32.64%), Space: 68.7 MB (63.36%) - LeetHub

---
+++ maximum-size-subarray-sum-equals-k/maximum-size-subarray-sum-equals-k.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int maxSubArrayLen(int[] nums, int k) {
+        
+        int sum =0;
+        int max = 0;
+        Map<Integer, Integer> map = new HashMap();
+        for (int i = 0 ; i < nums.length ; i++){
+            
+            sum+= nums[i];
+            if(sum==k)
+                max=Math.max(max,i+1);
+            
+            else if(map.containsKey(sum-k)){
+                max = Math.max(max, i- map.get(sum-k));
+            }
+            
+                if(!map.containsKey(sum))
+                    map.put(sum,i);
+        }
+        return max;
+    }
+}
