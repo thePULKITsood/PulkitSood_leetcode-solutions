e339e4512ccd9d2e6d5aad65c64d80b9c3d76442
pulkit sood <59098007+thePULKITsood@users.noreply.github.com>
2021-08-27 23:35:35 +0530

    Time: 8 ms (21.81%), Space: 64.8 MB (6.94%) - LeetHub

---
+++ count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int countSquares(int[][] matrix) {
+    int r = matrix.length;
+        int c = matrix[0].length;
+        int ans =0;
+        for (int i=0;i<r;i++){
+            for (int j=0;j<c;j++){
+                if(i==0||j==0){
+                    ans+=matrix[i][j];
+              }
+                else if(matrix[i][j]==1){
+                    int left = matrix[i][j-1];
+                    int top  = matrix[i-1][j]; 
+                    int diag  = matrix[i-1][j-1];
+                    int min = Math.min(diag,Math.min(left,top));
+                    matrix[i][j]+=min;
+                    ans+= matrix[i][j];
+                }
+            }
+        }
+        return ans;
+    }
+}
+// basically for a cell to be a bottom left corner of a cube 
+// the left top and diagonal must also be a part of it 
+// basically i will note how many squares is the  onw part of 
+/*
+[0,1,1,1],
+[1,1,2,2],
+[0,1,2,3]
+
+*/
