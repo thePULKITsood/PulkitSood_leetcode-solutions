6b8a284a96664b2ebe86ae60162d46e14ae2df89
pulkit sood <59098007+thePULKITsood@users.noreply.github.com>
2021-09-01 01:48:48 +0530

    Time: 59 ms (5.04%), Space: 43.6 MB (5.69%) - LeetHub

--- car-pooling/car-pooling.java
+++ car-pooling/car-pooling.java
@@ -1,14 +1,15 @@
 class Solution {
     public boolean carPooling(int[][] trips, int capacity) {
-        TreeMap <Integer, Integer> map = new TreeMap<>();
+        int time [] = new int [10001];
         
         for (int t [] : trips){
-            map.put(t[1],map.getOrDefault(t[1],0)+ t[0]);
-            map.put(t[2],map.getOrDefault(t[2],0)- t[0]);
+            
+            time[t[1]]+= t[0];
+            time[t[2]]-= t[0];
             
             int sum=0;
-            for (int i : map.keySet()){
-                sum+=map.get(i);
+            for (int i : time){
+                sum+= i;
                 
                 if(sum> capacity)
                     return false;
