65184216c6e2297657e3170a0c10c2c254700826
pulkit sood <59098007+thePULKITsood@users.noreply.github.com>
2021-09-08 17:06:34 +0530

    Time: 12 ms (80.44%), Space: 39.9 MB (64.74%) - LeetHub

---
+++ as-far-from-land-as-possible/as-far-from-land-as-possible.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int maxDistance(int[][] grid) {
+        int n=grid.length;
+        int m=grid[0].length;
+        
+        Queue<int[]> q=new LinkedList();
+        
+        for (int i=0;i<n;i++){
+            for (int j=0;j<m;j++){
+                if(grid[i][j]==1)
+                    q.add(new int []{i ,j});
+       }
+    }
+    if(q.size()==0 || q.size()==m*n) return -1;    
+        
+        int dis=0;
+        int dir[][]={{0,1},{0,-1},{-1,0},{1,0}};
+        while (q.size()>0){
+            int size =q.size();
+            while (size-->0){
+                int []t =q.remove();
+                
+                 //add all the neighbours of t
+                for (int d[]:dir){
+                    int newi= t[0]+d[0];
+                    int newj =t[1]+d[1];
+                        
+                        if(newi>=0 && newj >=0 && newi <grid.length && newj<grid[0].length && grid[newi][newj]==0)
+                        {
+                            grid[newi][newj]=1;
+                            q.add(new int []{newi ,newj});
+                }
+                }
+                
+            }
+            dis++;
+        }
+        return dis-1;
+    }
+}
