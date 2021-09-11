672207f7738a4de7109c040c9b5ede1b1804535f
pulkit sood <59098007+thePULKITsood@users.noreply.github.com>
2021-09-11 15:24:25 +0530

    Time: 18 ms (12.12%), Space: 45.3 MB (13.07%) - LeetHub

--- minimum-number-of-taps-to-open-to-water-a-garden/minimum-number-of-taps-to-open-to-water-a-garden.java
+++ minimum-number-of-taps-to-open-to-water-a-garden/minimum-number-of-taps-to-open-to-water-a-garden.java
@@ -1,44 +1,21 @@
 class Solution {
     public int minTaps(int n, int[] ranges) {
-        int range [][]= new int [n+1][2];
-        for (int i=0;i<=n;i++){
-            int l =Math.max(0, i-ranges[i]);
-            int r = i+ranges[i];
-            
-            range[i][0]=l;
-            range[i][1]=r;
-        }
-        // Arrays.sort(range,(a,b)->a[0]-b[0]);
-          Arrays.sort(range,(a,b)-> a[0]==b[0]?(b[1]-b[0] - a[1]+a[0]) :a[0]-b[0]);
-        
-        int start =range[0][0];
-        int end = range[0][1];
-        
-        int max=end;
-        int maxstart=start;
-        
-        if(start>0)return -1;
-        int ans=1;
-        
-        while (end<n){
-            for (int i=0;i<=n;i++){
-                if(range[i][0]>=start && range[i][0]<=end){
-                    if (range[i][1] > max){
-                        maxstart=range[i][0];
-                        max = range[i][1];
-                    }
+       
+        int min=0;
+        int max=0;
+        int open =0;
+        while (max<n){
+            for (int i=0;i<ranges.length;i++){
+                if( i-ranges[i]<=min && i+ranges[i]>max ){
+                    max=i+ranges[i];
                 }
+              
             }
-            if(max==end)return -1;
-            end=max;
-            start=maxstart;
-            // if(maxstart==0)ans--;
-            ans++;
-            
-            
+              if(min==max)return -1;
+                min=max;
+                open++;
         }
-        return ans;
-        
+        return open;
         
     }
 }
