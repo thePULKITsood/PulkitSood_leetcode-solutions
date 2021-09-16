60cec0b39f1d721d375333a0c476f904a142ed51
pulkit sood <59098007+thePULKITsood@users.noreply.github.com>
2021-09-16 19:57:11 +0530

    Time: 109 ms (10.21%), Space: 41.3 MB (26.07%) - LeetHub

--- longest-increasing-subsequence/longest-increasing-subsequence.java
+++ longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -3,28 +3,31 @@ class Solution {
         
         int n = nums.length;
         int dp [] =new int [n];
+           int omax = 1;
         for (int i = 0 ; i < n ; i++){
             
             int max =1 ;
             
-            for (int j=0;j<i;j++){
+            for (int j=i-1;j>=0;j--){
                 
-                if (nums[i]>nums[j]){
+                if (nums[i]>nums[j]  ){
                     
                     max = Math.max(dp[j] + 1 , max);
                     
                 }
             }
             dp[i]=max;
+            if (dp[i] > omax)
+                omax = dp[i];
         }
-        int max = 0;
-        for (int a : dp ){
-            max = Math.max(a,max);
-        }
-        return max;
+     
+       
+        return omax;
     }
 }
 
 // for each point  i will store the value of the longest inc subsequence that ends on that point 
 
 // for a new point i will traverse all its left parts and if the point is valid  i will check my max and update my max accordinglt and 
+
+// n2 solution 
