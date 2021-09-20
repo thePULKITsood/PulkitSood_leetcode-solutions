9948518a4941e80e47d01c6039e13ed4f2f2463e
pulkit sood <59098007+thePULKITsood@users.noreply.github.com>
2021-09-20 23:09:11 +0530

    Time: 311 ms (17.87%), Space: 46.9 MB (27.47%) - LeetHub

--- russian-doll-envelopes/russian-doll-envelopes.java
+++ russian-doll-envelopes/russian-doll-envelopes.java
@@ -1,5 +1,6 @@
 class Solution {
     public int maxEnvelopes(int[][] env) {
+        
         Arrays.sort(env,(a,b)->{
             if(a[0]==b[0])return a[1]-b[1];
             else return a[0]-b[0];
@@ -7,10 +8,15 @@ class Solution {
        
         int n=env.length;
         int dp[]=new int [n];
+        
         Arrays.fill(dp,1);
+        
         for (int i=1;i<n;i++){
+            
             int max =0;
+            
             for (int j=i-1;j>=0;j--){
+                
                 if(env[i][0]>env[j][0] && env[i][1]>env[j][1])
                     max=Math.max(dp[j],max);
             }
