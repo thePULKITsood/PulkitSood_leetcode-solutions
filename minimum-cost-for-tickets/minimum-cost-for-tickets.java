3f8959341a146b576090dd1f12c3ae47adfb4591
pulkit sood <59098007+thePULKITsood@users.noreply.github.com>
2021-09-28 11:12:58 +0530

    Time: 1 ms (83.63%), Space: 37 MB (70.24%) - LeetHub

--- minimum-cost-for-tickets/minimum-cost-for-tickets.java
+++ minimum-cost-for-tickets/minimum-cost-for-tickets.java
@@ -1,35 +1,23 @@
 class Solution {
- 
-    HashSet <Integer> daySet=new HashSet();
-    int n;
-    int memo[];
+     public int mincostTickets(int[] days, int[] costs) {
+    // length up to the last travel + 1 day is good enough (no need for 365)
+    int lastDay = days[days.length - 1]; 
+    // dp[i] means up to i-th day the minimum cost of the tickets
+    int[] dp = new int[lastDay + 1]; 
+    boolean[] isTravelDays = new boolean[lastDay + 1];
+    // mark the travel days
+    for(int day : days) isTravelDays[day] = true;
     
-    public int mincostTickets(int[] days, int[] cost) {
-        n = days.length;
-        for (int d : days)daySet.add(d);
-    
-        memo = new int [days[n-1]+1];
-        
-        return rec (days[0],cost,days);
-        
-    }
-    public int rec (int day , int []cost, int days[]){
- 
-        if(day>days[n-1]) return 0;
-        if(memo[day]!=0)return memo[day];
-        int ans= Integer.MAX_VALUE;
-        
-        if(daySet.contains(day)){
-        
-            int one =rec(day+1 , cost, days) + cost[0];
-            int week = rec(day+7,cost,days)+cost[1];
-            int month =rec(day+30,cost,days)+cost[2];
-            
-             ans = Math.min(one,Math.min(week,month));
+    for(int i = 1; i <= lastDay; i++) {
+        if(!isTravelDays[i]) { // no need to buy ticket if it is not a travel day
+            dp[i] = dp[i - 1];
+            continue;
         }
-        else return rec(day+1,cost,days);
-        
-        return memo[day]= ans;
-        
+        // select which type of ticket to buy
+        dp[i] = costs[0] + dp[i - 1]; // 1-day
+        dp[i] = Math.min(costs[1] + dp[Math.max(i - 7, 0)], dp[i]); // 7-day
+        dp[i] = Math.min(costs[2] + dp[Math.max(i - 30, 0)], dp[i]); // 30-day
     }
+    return dp[lastDay];
+}
 }
