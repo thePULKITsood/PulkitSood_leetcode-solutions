28ec85acd20dea766658a3cf92d75521b52b2661
pulkit sood <59098007+thePULKITsood@users.noreply.github.com>
2021-10-03 14:36:02 +0530

    Time: 1 ms (100.00%), Space: 40 MB (50.24%) - LeetHub

--- jump-game/jump-game.java
+++ jump-game/jump-game.java
@@ -1,21 +1,29 @@
 class Solution {
     public boolean canJump(int[] nums) {
         int n = nums.length;
-        boolean[] arr = new boolean[n];
+//         boolean[] arr = new boolean[n];
         
-        arr[n-1]=true;
-        for (int i = n-2;i>=0;i--){
+//         arr[n-1]=true;
+//         for (int i = n-2;i>=0;i--){
             
-            int dis = nums[i];
-            for (int j = i+1; j <= i+dis && j<n ; j++){
-                if(arr[j]==true)
-                {
-                    arr[i]=true;
-                    break;
-                }
-            }
+//             int dis = nums[i];
+//             for (int j = i+1; j <= i+dis && j<n ; j++){
+//                 if(arr[j]==true)
+//                 {
+//                     arr[i]=true;
+//                     break;
+//                 }
+//             }
             
+//         }
+//         return arr[0];
+        
+        int last = n-1;
+        
+        for (int i = n-2;i>=0;i-- ){
+            if(i+nums[i]>=last)
+                last=i;
         }
-        return arr[0];
+        return last==0;
     }
 }
