7f4ff476854b2a07c1bc718cf85863d96612ad84
pulkit sood <59098007+thePULKITsood@users.noreply.github.com>
2021-10-08 10:41:14 +0530

    Time: 29 ms (95.54%), Space: 48.1 MB (86.63%) - LeetHub

--- implement-trie-prefix-tree/implement-trie-prefix-tree.java
+++ implement-trie-prefix-tree/implement-trie-prefix-tree.java
@@ -1,51 +1,41 @@
 class Trie {
 
     private class Node {
-        Node[]children;
+        Node [] children;
         boolean isEnd;
         
-        
-        Node (){
-            children = new Node [26];
+        Node () {
+             children = new Node [26];
         }
     }
+    
     final private Node root;
     
-    /** Initialize your data structure here. */
-  
     public Trie() {
-        root = new Node();
-        
+        root = new Node();   
     }
-    
-    /** Inserts a word into the trie. */
-    
-    
+   
     public void insert(String word) {
         
         Node curr = root ;
-        
         for (int i = 0 ;i < word.length() ; i++){
             char ch = word.charAt(i);
-            
             if (curr.children [ ch-'a'] == null ){
 //                 means this char is not present 
 //                 so insert it there 
-                curr.children [ch-'a'] = new Node ();
-                
+                curr.children [ch-'a'] = new Node ();   
             }
+            // now if it was not there i have inserted it and if it was there i have not run that statement and i am going to move my curr to the next child - either the newly inserted on e or the old one 
             
-//             now if it was not there i have inserted it and if it was there i have not run that statement and i am going to move my curr to the next child - either the newly inserted on e or the old one 
             curr = curr.children[ch-'a'];
         }
+        
 //         when this loop ends i will mark the  curr.isEnd = true;
         curr.isEnd = true;
         
     }
-    
-    /** Returns if the word is in the trie. */
+  
     public boolean search(String word) {
-        
         Node curr = root;
         for (int i = 0 ; i < word.length() ; i++){
             char ch = word.charAt(i);
@@ -60,9 +50,7 @@ class Trie {
         
     }
     
-    /** Returns if there is any word in the trie that starts with the given prefix. */
     public boolean startsWith(String prefix) {
-           
         Node curr = root;
         for (int i = 0 ; i < prefix.length() ; i++){
             char ch = prefix.charAt(i);
@@ -70,8 +58,7 @@ class Trie {
                 return false ;
             }
 //             matlab wo null nahi hai so move forward
-            curr = curr.children[ch-'a'];
-            
+            curr = curr.children[ch-'a'];     
         }
         return true;
        
