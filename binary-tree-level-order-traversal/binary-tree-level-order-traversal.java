203abed1e3796badcce52ebf9e5c82996ae91dd4
pulkit sood <59098007+thePULKITsood@users.noreply.github.com>
2021-10-14 02:38:20 +0530

    Time: 2 ms (26.19%), Space: 40.2 MB (26.40%) - LeetHub

--- binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
+++ binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
@@ -14,44 +14,25 @@
  * }
  */
 class Solution {
-    
-    List<List<Integer>> ans = new ArrayList();
-    
-    public List<List<Integer>> levelOrder(TreeNode root) 
-    {
-        if (root == null)return ans;
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> ans = new ArrayList();
+         if (root == null)return ans;
         
-        LinkedList<TreeNode> q = new LinkedList<>();
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
         
-        help(root , q);
+        while (!q.isEmpty()){
+            int size = q.size();
+            List<Integer> minans = new ArrayList();
+            while (size-- >0){
+                TreeNode temp = q.remove();
+                 minans.add(temp.val);
+                if (temp.left != null)q.add(temp.left);
+                if (temp.right != null)q.add(temp.right);
+            
+            }
+             ans.add(minans);
+        }
         return ans;
     }
-    
-    public void help(TreeNode root ,LinkedList<TreeNode> q)
-    {
-        q.addLast(root);
-        
-        while (q.size()>0)
-            
-        { 
-            List<Integer> minans = new ArrayList();
-            int size = q.size() ;
-     
-            while (size -- >0)
-            {
-//             remove 
-                TreeNode temp = q.removeFirst();
-            
-//             work
-                minans.add(temp.val);
-            
-//             add Children  - we have to see they must not be null 
-                if (temp.left != null)q.addLast(temp.left);
-                if (temp.right != null)q.addLast(temp.right);
-            
-        }
-            ans.add(minans);
-    }
-    }
-    
 }
