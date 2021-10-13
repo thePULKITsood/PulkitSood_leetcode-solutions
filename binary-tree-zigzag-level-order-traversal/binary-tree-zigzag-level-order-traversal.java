282f725cf9613c39dc370265069e6ed8e302497c
pulkit sood <59098007+thePULKITsood@users.noreply.github.com>
2021-10-14 02:47:58 +0530

    Time: 3 ms (7.00%), Space: 39.2 MB (46.20%) - LeetHub

--- binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
+++ binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
@@ -18,29 +18,17 @@ class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
         
     if (root == null )return ans    ;
-        // when level is /2 then left to right other wise go from right ot left 
-        
-        // here we will not use queue we will use stack because it will hamper our order and we do not want a mixed bag 
-          // now in stack if level /2 --> i will add  left to right so that my choldren comes out right to left 
-          // for level 1 i will add from right ot left and when i will pop then the order will be the exact ewhich i want 
-            // dry run bitch 
-        
-        LinkedList<TreeNode> s = new LinkedList();
+     
+        Stack <TreeNode> s = new Stack();
         helper(root,s);
         return ans;
     }
-  public  void  helper (TreeNode root ,  LinkedList<TreeNode> s )
+  public  void  helper (TreeNode root ,  Stack <TreeNode> s )
     {
         int level = 0 ;
-        s.addFirst(root);
-      LinkedList<TreeNode> cs = new LinkedList();
-//       i have to keep a child stack dry run and know why 
-//       beacuse when i remove one value and ad its children then its sibling is left below 
-        
-        
-        
-        // now i will begin 
-        
+        s.push(root);
+      Stack <TreeNode> cs = new Stack();
+
         while (s.size()> 0 )
         {
              int size = s.size();
@@ -48,28 +36,26 @@ class Solution {
             
               while (size -- >0)
             {
-                
-                TreeNode temp = s.removeFirst();
-                  
+                TreeNode temp = s.pop();
                 minans.add(temp.val);
                 
                 if (level % 2 == 0)
                 {
                     // add left to right in the stack 
                     
-                    if (temp.left != null) cs.addFirst(temp.left);
-                     if (temp.right != null) cs.addFirst(temp.right);
+                    if (temp.left != null) cs.push(temp.left);
+                     if (temp.right != null) cs.push(temp.right);
                 }
                 
                  if (level %2 == 1)
                     {
                     // add rightto left  in the stack                
-                    if (temp.right != null) cs.addFirst(temp.right);
-                    if (temp.left != null) cs.addFirst(temp.left); 
+                    if (temp.right != null) cs.push(temp.right);
+                    if (temp.left != null) cs.push(temp.left); 
                 }
             }
             s =cs;
-            cs = new LinkedList();
+            cs = new Stack();
             level ++ ;
             ans.add(minans);
         }
