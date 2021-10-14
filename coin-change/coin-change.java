2c5be3f01a4e4104c874151665d614f51eea3c00
pulkit sood <59098007+thePULKITsood@users.noreply.github.com>
2021-10-15 04:46:55 +0530

    Time: 25 ms (45.07%), Space: 41.8 MB (32.67%) - LeetHub

--- coin-change/coin-change.java
+++ coin-change/coin-change.java
@@ -1,30 +1,24 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        int dp [] = new int [amount + 1 ];
-        
+        int dp [] = new int [amount +1];
         Arrays.fill(dp,-1);
-        for(int coin : coins)
-            if (coin <=amount)
-                dp[coin] = 1 ;
+        for (int c : coins)
+            if(c<=amount)
+            dp[c]=1;
         
         dp[0]=0;
-        
-        for (int i = 1 ; i<=amount ; i++){
-            if(dp[i]==1)
-                continue;
-            
+        for (int i=1;i<=amount ; i++)
+        {
+            if(dp[i]==1 )continue;
             int ans = Integer.MAX_VALUE;
-            for(int coin : coins){
-                if(i-coin > 0 && dp[i-coin]>0)
-                {
-                    ans=Math.min(ans,dp[i-coin]+1);
+            for (int c : coins){
+                if(i-c >0 && dp[i-c]>0){
+                    ans=Math.min(ans,dp[i-c]+1);
                 }
             }
             if(ans!=Integer.MAX_VALUE)
                 dp[i]=ans;
-            
         }
         return dp[amount];
-        
     }
 }
