e95f2cb584ff2047f1161855c1a8da9b836dd07f
pulkit sood <59098007+thePULKITsood@users.noreply.github.com>
2021-10-17 12:22:01 +0530

    Time: 0 ms (100.00%), Space: 38.9 MB (63.78%) - LeetHub

--- find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.java
+++ find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.java
@@ -3,7 +3,7 @@ class Solution {
       
         // if left ot mid is sorted then ans will be from left = mid + 1 to high 
         
-        //else if mid to hig  is sorted ans will be from left ot mid = high 
+        //else if mid to hig  is sorted ans will be from left to mid = high 
         
         int n = nums.length;
         if(n==1)return nums[0];
