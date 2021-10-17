74f760ce19fd4d4d147cee4cebfa5788831afb06
pulkit sood <59098007+thePULKITsood@users.noreply.github.com>
2021-10-17 15:42:45 +0530

    Time: 7 ms (42.12%), Space: 49.3 MB (89.58%) - LeetHub

--- best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
+++ best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
@@ -1,50 +1,29 @@
 class Solution {
-    public int maxProfit(int[] p) {
-//         approach by - sumit sir == bidirectional dp 
- 
-//         filling the left dp 
-        int n = p.length;
+    public int maxProfit(int[] prices) {
+        int days=prices.length;
+        int dp[][] = new int [3][days];
         
-        int leftdp[] = new int [n+1];
-        int lmine =p[0];
-        
-        for (int i = 1 ; i < n ; i++){
-            
-            if (p[i] < lmine){
-                lmine = p[i];
+        for (int t=1;t<=2;t++){
+            int prevDay = -prices[0];
+            for (int d=1;d<days;d++){
+                // this value will be formed by companrion
+                
+
+//                 1. of the last day and same transaction
+//                 2. off all the days before this day and one less transaction and the other transaction happening between that day and this day
+                
+                int  lastday = dp[t][d-1];
+                int max=Math.max(lastday,prevDay+prices[d]);
+                
+               
+                dp[t][d]=max;
+                prevDay=Math.max(prevDay,dp[t-1][d]-prices[d]);
+                
+                
             }
-            leftdp [i]  =Math.max(leftdp [i-1] , p[i] - lmine); 
         }
+            return dp[2][days-1];
         
-        int rightdp[] = new int [n+1];
-       
-        int rmaxe =p[n-1];
         
-        for (int i = n-2 ; i>= 0 ;i--){
-            
-            if (p[i] > rmaxe){
-                rmaxe = p[i];
-            }
-             
-            
-            rightdp [i]  =Math.max(rightdp[i+1], rmaxe -p[i] ); 
-        }
-        int maxprofit = 0;
-        for (int i = 0 ; i < n + 1 ;i++){
-            maxprofit = Math.max(maxprofit,leftdp[i]+rightdp[i]);
-        }
-        
-        return maxprofit;
     }
 }
-
-
-//in the left dp i am storing  the max profit i can make by buying and selling on the left side 
-
-//for this i have to check the current max and the profit made by selling today and update the maximum 
-
- // for the right dp i have to check themax profit htat can be made on the right by buying and selling 
-
-// for that i check the max of max and the profit if i buy today and sell on a higher price later 
-
-// they are basically reverse of each other 
