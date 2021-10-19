4e181512e9598a787bd6f78b21ef7dd5e3a519b2
pulkit sood <59098007+thePULKITsood@users.noreply.github.com>
2021-10-19 19:26:06 +0530

    Time: 46 ms (25.70%), Space: 51.5 MB (39.49%) - LeetHub

--- trapping-rain-water-ii/trapping-rain-water-ii.java
+++ trapping-rain-water-ii/trapping-rain-water-ii.java
@@ -1,20 +1,21 @@
 class Solution {
+    // https://www.youtube.com/watch?v=cJayBq38VYw
     public int trapRainWater(int[][] grid) {
+        
         int r = grid.length;
         int c = grid[0].length;
         
         //arr == height row col 
-        
+    
         PriorityQueue<int []> heap = new PriorityQueue<>((a,b)-> a[0]-b[0]);
         boolean [][] vis = new boolean[r][c];
         
-        
-        for (int i = 0 ; i<r;i++){
-            
+        for (int i = 0 ; i<r;i++){            
             for (int j=0;j<c ; j++){
                 
                 if(i==0|| i==r-1 || j==0 || j==c-1){
-                            int val = grid[i][j];
+                    
+                        int val = grid[i][j];
                         heap.add(new int [] {val,i,j});
                         vis[i][j]=true;
                 }
@@ -23,16 +24,21 @@ class Solution {
        
         int vol = 0;
         int minbound=0;
+        
         int dir[][]={{0,1},{0,-1},{1,0},{-1,0}};
+        
         while (heap.size()>0){
+            
             int current=heap.peek()[0];
             int row = heap.peek()[1];
             int col =heap.peek()[2];
+            
             minbound = Math.max(minbound,current);
             heap.remove();
             
             for (int d[] : dir){
-                int rr=row+d[0];
+                
+                int rr = row + d[0];
                 int cc = col + d[1];
                 
                 if(rr>=0 && cc>=0 && rr<r && cc<c && !vis[rr][cc]){
@@ -42,20 +48,10 @@ class Solution {
                         vis[rr][cc]=true;
                     
                     if(val<minbound)
-                    {
-                        vol+=minbound-val;
-                    }                
+                      vol+=minbound-val;
                 }
-                
             }
-            
-            
         }
-        
-        
-        
-        
-        return vol;
-        
+         return vol;
     }
 }
