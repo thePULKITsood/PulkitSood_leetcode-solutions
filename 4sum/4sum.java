845d436da40d3d08b2ae52bf1fc858a88c08bd31
pulkit sood <59098007+thePULKITsood@users.noreply.github.com>
2021-10-19 15:03:54 +0530

    Time: 196 ms (13.17%), Space: 114.5 MB (5.31%) - LeetHub

--- 4sum/4sum.java
+++ 4sum/4sum.java
@@ -1,52 +1,36 @@
+
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
         int n = nums.length;
         Arrays.sort(nums);
-        // Set<List<List<Integer>>> set = new HashSet();
+        Set<List<List<Integer>>> set = new HashSet();
         
-        List<List<Integer>> ans = new ArrayList();
-        
+        // List<List<Integer>> ans = new ArrayList();
         for (int i = 0 ; i< n  ; i++){
-            
-            for (int j = i+1 ; j <n ; j++){
-                
-                int l = j+1;
+            for (int k = i+1 ; k<n ; k++){
+                int l = k+1;
                 int h = n-1;
-                
-                while (l<h)
-                {
-                    int sum = nums[i] + nums[j] + nums[l] + nums[h];
-                
+                while (l<h){
+                    int sum = nums[i] + nums[k] + nums[l] + nums[h];
                     if(sum==target){
                         List<Integer> add = new ArrayList();
                         add.add(nums[i]);
-                        add.add(nums[j]);
+                        add.add(nums[k]);
                         add.add(nums[l]);
                         add.add(nums[h]);
                         
-                        ans.add(new ArrayList(add));
-                        
-                        // remove all the duplicates 
-                        
-                        while (l<h && nums[l]== add.get(2)) l++;
-                        
-                        while (l<h && nums[h]== add.get(3)) h--;
-                        
-                       
+                        set.add(new ArrayList(add));
+                        l++;
+                        h--;
                     }
                     if(sum>target)
                         h--;
-                    else if(sum<target)
+                      else if(sum<target)
                         l++;
                 }
-                //now we will move the j pointer
-//                 the main thing is moving the j pointer after one while loop is done 
-                
-                while (j+1<n && nums[j+1] == nums[j]) j++;
             }
-             while (i+1<n && nums[i+1] == nums[i]) i++;
         }
-         // List<List<Integer>> ans = new ArrayList(set);
+         List<List<Integer>> ans = new ArrayList(set);
         return ans;
     }
 }
