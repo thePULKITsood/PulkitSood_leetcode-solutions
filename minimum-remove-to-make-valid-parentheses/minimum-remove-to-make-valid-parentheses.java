4a40cca203d014ca807af6e659f189167c63180c
pulkit sood <59098007+thePULKITsood@users.noreply.github.com>
2021-10-23 05:42:08 +0530

    Time: 8 ms (99.08%), Space: 39.3 MB (98.25%) - LeetHub

--- minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.java
+++ minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.java
@@ -1,35 +1,35 @@
 class Solution {
     public String minRemoveToMakeValid(String s) {
-        Stack<Integer> st  = new Stack ();
-//         we will make an array or map of indexes where we will store the things which are not to be added in the final string 
+        int open =0;
+        char arr [] =s.toCharArray();
+        for (int i=0;i<s.length();i++){
+            if(arr[i]=='(')
+                open++;
+            
+        if(arr[i]==')')
+        {
+            if(open==0)
+                arr[i]=' ';
+            else 
+                open--;
+        }
+            
+        }
+        for (int i=s.length()-1;i>=0 && open >0 ;i--){
+            if(arr[i]=='('){
+                open--;
+                arr[i]=' ';
+            }
+        }
+        StringBuilder sb =new StringBuilder();
         
-        for (int i = 0; i< s.length() ; i++){
-            
-            char ch = s.charAt(i);
-            if (ch=='('){
-                st.push(i);
+        for (int i=0;i<s.length();i++){
+            if(arr[i]!=' ')
+                sb.append(arr[i]);    
             }
-            
-            else if (ch==')'){
-                if (st.isEmpty())
-                    st.push(i);
-                else if (s.charAt(st.peek()) =='(')
-                {
-                    st.pop();
-                }
-                else st.push(i);
-            }
-            
-        }
-        Set<Integer> indexesToRemove = new HashSet<>();
-         while (!st.isEmpty()) indexesToRemove.add(st.pop());
-                StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < s.length(); i++) {
-            if (!indexesToRemove.contains(i)) {
-                sb.append(s.charAt(i));
-            }
-        }
-        return sb.toString();
+        
+    return sb.toString();
+        
         
     }
 }
