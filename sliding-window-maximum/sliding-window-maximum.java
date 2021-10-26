18402df412da37a6f0ee3d3c83d70c85080499c5
pulkit sood <59098007+thePULKITsood@users.noreply.github.com>
2021-10-26 08:00:03 +0530

    Time: 55 ms (41.13%), Space: 132.6 MB (18.18%) - LeetHub

--- sliding-window-maximum/sliding-window-maximum.java
+++ sliding-window-maximum/sliding-window-maximum.java
@@ -1,31 +1,40 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-        Deque<Integer> dq = new ArrayDeque<>();
         int n = nums.length;
+        int ans [] = new int [n-k+1];
         
-        int ans[] = new int [n-k+1];
-        int right =0;
+        Deque<Integer> q = new ArrayDeque();
         
-        for (int i=0;i<n;i++){
-            // remove the elements whic are at the front and no longer part of the sliding window 
-            while (dq.size()>0 && dq.peek() <= i-k)
-                dq.poll();
-            
-            
-            //no we have only the elements which are part of this sliding window
-            //now we will remove all the elements smaller than the current element from the deque
-            
-            while (dq.size()>0 && nums[dq.peekLast()]<nums[i])
-                dq.pollLast();
-            
-            dq.offer(i);
-            
-            
-            if(i>=k-1){
-                ans[right++]=nums[dq.peek()];
-            }
+        int i=0;
+        int idx=0;
+        
+        while (i<n){
+        // it will store the minimum alues 
+        
+        // step 1 remove all the values form the fromt which are not relevant to thid windoe 
+        
+        while (!q.isEmpty() && q.peek()<= i-k  ){
+            q.poll();
         }
-        return ans;
         
+        
+        // step 2 remove all values from the top which are smaller than the current value 
+        // we are doing this as the q now contains only the values that are for this wondow and the values which are smaleer will never be used so we remove smaller values 
+        while (!q.isEmpty() && nums[q.peekLast()]< nums[i]  ){
+            q.pollLast();
+        }
+        
+        q.offer(i);
+        
+        
+        
+        
+        // step 3-  we update the answer array 
+        if(i>=k-1)
+            ans[idx++]=nums[q.peek()];
+        
+        i++;
+        }
+    return ans;
     }
 }
