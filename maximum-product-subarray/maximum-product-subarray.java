71706f25ab30919025e13d626c23f4bb578def9a
pulkit sood <59098007+thePULKITsood@users.noreply.github.com>
2021-10-30 07:11:49 +0530

    Time: 0 ms (100.00%), Space: 38.9 MB (58.84%) - LeetHub

--- maximum-product-subarray/maximum-product-subarray.java
+++ maximum-product-subarray/maximum-product-subarray.java
@@ -1,21 +1,20 @@
 class Solution {
     public int maxProduct(int[] nums) {
-        if (nums.length == 0) return 0;
-
-        int max_so_far = nums[0];
-        int min_so_far = nums[0];
-        int result = max_so_far;
-
-        for (int i = 1; i < nums.length; i++) {
-            int curr = nums[i];
-            int temp_max = Math.max(curr, Math.max(max_so_far * curr, min_so_far * curr));
-            min_so_far = Math.min(curr, Math.min(max_so_far * curr, min_so_far * curr));
-
-            max_so_far = temp_max;
-
-            result = Math.max(max_so_far, result);
+        int max = Integer.MIN_VALUE;
+        int prod = 1;
+        
+        for (int n : nums){
+            prod*=n;
+            max=Math.max(max,prod);
+            if(prod==0)prod=1;
+            
         }
-
-        return result;
+        prod=1;
+        for (int i=nums.length-1; i>=0;i--){
+            prod*= nums[i];
+            max=Math.max(max,prod);
+            if(prod==0)prod=1;
+        }
+        return max;
     }
 }
