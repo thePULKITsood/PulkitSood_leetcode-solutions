61aed28a25e0e96c521de472921804985123de23
pulkit sood <59098007+thePULKITsood@users.noreply.github.com>
2021-10-30 07:41:15 +0530

    Time: 4 ms (90.27%), Space: 39 MB (52.14%) - LeetHub

--- longest-increasing-subsequence/longest-increasing-subsequence.java
+++ longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -3,27 +3,42 @@ class Solution {
         int n = nums.length;
         ArrayList<Integer> ans = new ArrayList();
         ans.add(nums[0]);
+        
         for (int i=1;i<n;i++){
-            if(nums[i]>ans.get(ans.size()-1)){
+            int last = ans.get(ans.size()-1);
+            
+            if(nums[i]>last)
                 ans.add(nums[i]);
-            }
+                
+            
             else {
                 //do a binary search and find the element just bigger than or equal to this one
-                int j = search(ans,nums[i]);
+                
+                int j=Collections.binarySearch(ans,nums[i]);
+                if(j>=0)
                 ans.set(j,nums[i]);
+                else {
+                    j*=-1;
+                    j-=1;
+                    ans.set(j,nums[i]);
+                }
             }
         }
         return ans.size();
     }
-    int search(ArrayList<Integer> ans , int x){
-        int left=0;
-         int right= ans.size()-1;
-        while (left<right){
-            int mid = (left+right)/2;
-            if(ans.get(mid)==x)return mid;
-            else if (ans.get(mid)<x) left = mid+1;
-            else right =mid;
-        }
-        return left;
-    }
+    
+//     int search(ArrayList<Integer> ans , int x){
+//          int left=0;
+//          int right= ans.size()-1;
+     
+//         while (left<right){
+            
+//             int mid = (left+right)/2;
+//             if(ans.get(mid)==x)return mid;
+            
+//             else if (ans.get(mid)<x) left = mid+1;
+//             else right =mid;
+//         }
+//         return left;
+//     }
 }
