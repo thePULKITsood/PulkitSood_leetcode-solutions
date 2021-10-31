1aacef572b0a5ddaf7c2a59777ae8749155961f7
pulkit sood <59098007+thePULKITsood@users.noreply.github.com>
2021-10-31 14:49:32 +0530

    Time: 88 ms (71.74%), Space: 86.3 MB (73.63%) - LeetHub

--- the-number-of-weak-characters-in-the-game/the-number-of-weak-characters-in-the-game.java
+++ the-number-of-weak-characters-in-the-game/the-number-of-weak-characters-in-the-game.java
@@ -12,7 +12,9 @@ class Solution {
       int max1=arr[0][0];
        int max2= arr[0][1];
         for (int per [] : arr){
-            if(per[0]< max1){
+            
+            if(per[0] < max1){
+                
                 if(per[1]<max2)
                     ans++;
             }
@@ -21,35 +23,11 @@ class Solution {
         return ans;
     }
 }
+// [[7,9],[10,7],[6,9],[10,4],[7,5],[7,10]]
 
-/*
-[[7,7],[1,2],[9,7],[7,3],[3,10],[9,8],[8,10],[4,3],[1,5],[1,5]]
-10 6  9 5   8 4   8 5   8 7 
-
-
-[[1,1],[2,1],[2,2],[1,2]]
-2 2   2 1  1 2  1 1 
-
-[[7,9],[10,7],[6,9],[10,4],[7,5],[7,10]]
-
-
- 1  10  5 10   8  10    
- 1  9   4 9   6  9 
-              10 8      
-              9  6  
-
-1. sort on basis of atack
-
-2. then large attack -------  now 
-attack is same for all the next groups i need to get a max defence value and check how many have defence alue less than this attack value
-
-
-// meaniing 1. 
-sort on basis of attack   
-if attack same sort on baiss of defence 
-
-for the same attack do nothing and 
-have a max attack and keep updating the max defence and if any one falls below max attack and max defence add the numner 
-
-
-*/
+// 10 7   10 4  7 10  7 9   7 5  6 9    
+    
+    
+// 10 4   10 7  7 5  7 9   7 10  6 9    
+    
+    
