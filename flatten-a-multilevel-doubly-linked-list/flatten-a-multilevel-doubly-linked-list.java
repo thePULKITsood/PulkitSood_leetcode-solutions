c5a2f3dba3118cdbc6f3a583593cbb9e88407237
pulkit sood <59098007+thePULKITsood@users.noreply.github.com>
2021-10-31 14:39:13 +0530

    Time: 0 ms (100.00%), Space: 38.4 MB (46.77%) - LeetHub

--- flatten-a-multilevel-doubly-linked-list/flatten-a-multilevel-doubly-linked-list.java
+++ flatten-a-multilevel-doubly-linked-list/flatten-a-multilevel-doubly-linked-list.java
@@ -9,29 +9,39 @@ class Node {
 */
 
 class Solution {
-  public Node flatten(Node head) {
-    if (head == null) return head;
-    // pseudo head to ensure the `prev` pointer is never none
-    Node pseudoHead = new Node(0, null, head, null);
-
-    flattenDFS(pseudoHead, head);
-
-    // detach the pseudo head from the real head
-    pseudoHead.next.prev = null;
-    return pseudoHead.next;
-  }
-  /* return the tail of the flatten list */
-  public Node flattenDFS(Node prev, Node curr) {
-    if (curr == null) return prev;
-    curr.prev = prev;
-    prev.next = curr;
-
-    // the curr.next would be tempered in the recursive function
-    Node tempNext = curr.next;
-
-    Node tail = flattenDFS(curr, curr.child);
-    curr.child = null;
-
-    return flattenDFS(tail, tempNext);
-  }
+    public Node flatten(Node head) {
+        if(head==null)return head;
+        Node it = head;
+        
+        while (it!=null){
+            
+            if(it.child!=null){
+                Node next = it.next;
+                
+                it.next=flatten(it.child);
+                
+                //change pointers
+                it.next.prev=it;
+                it.child=null;
+            
+                
+                //now move to the last part of the flattened child list 
+                while (it.next!=null)
+                    it=it.next;
+                
+                // now to this part add the next 
+                
+                if(next!=null){
+                    it.next=next;
+                    it.next.prev=it;    
+                }
+                
+            }
+            
+            //if it is null then simply move 
+            it=it.next;
+        }
+        return head;
+        
+    }
 }
