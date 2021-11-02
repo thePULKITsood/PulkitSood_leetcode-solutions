30d62563b07976719a4e817506d6b793adfe6daa
pulkit sood <59098007+thePULKITsood@users.noreply.github.com>
2021-11-03 02:30:27 +0530

    Time: 346 ms (47.07%), Space: 0B (100.00%) - LeetHub

---
+++ nth-highest-salary/nth-highest-salary.sql
@@ -0,0 +1,9 @@
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+set N=N-1;
+  RETURN (
+     select distinct salary from Employee 
+      order by salary desc 
+      limit N,1
+  );
+END
