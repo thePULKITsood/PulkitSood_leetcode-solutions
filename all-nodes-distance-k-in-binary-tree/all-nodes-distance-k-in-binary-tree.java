50b34bbf9102ae125a2bfe67f756e006e21afd75
pulkit sood <59098007+thePULKITsood@users.noreply.github.com>
2021-11-04 19:31:29 +0530

    Time: 15 ms (64.73%), Space: 39.3 MB (49.88%) - LeetHub

--- all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.java
+++ all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.java
@@ -7,67 +7,10 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
-
- // store the parent in a hashmap and then just perform a bfs search according to level and then it will become easy 
-// this is basically a graph problem but as we can not go ackwards so we need ot make a mapping of parents to children so that we can go back and do a bfs -- so we create a hashmap 
-// node - left right and parent -- for parent we will use hashmap  and a visited set is also required 
-        
-
 class Solution {
-    
     Map<TreeNode ,TreeNode> map = new HashMap();
     List<Integer> res = new ArrayList();
     
-    public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
-       
-        if (root == null || k < 0) return res;
-         buildMap(root, null);
-        
-
-        Set <TreeNode> visited = new HashSet();
-        visited.add(null);
-        
-        LinkedList<TreeNode> q = new LinkedList();
-        q.addLast(target);
-        
-        int level = 0;
-        
-        while(q.size() > 0){
-            
-            if (level == k ){
-                while (q.size()>0){
-                    TreeNode temp = q.removeFirst();
-                    res.add(temp.val);
-                }
-                return res;
-            }
-            
-            int size = q.size();
-            while (size-- > 0){
-                
-                TreeNode temp = q.removeFirst();
-                visited.add(temp);
-                
-//                 addits 3 things 
-                
-                TreeNode parent = map.get(temp);
-                TreeNode left = temp.left;
-                TreeNode right = temp.right;
-                
-                if (! visited.contains(parent))q.addLast(parent);
-                if (! visited.contains(left))q.addLast(left);
-                if (! visited.contains(right))q.addLast(right);
-                
-                visited.add(parent);
-                visited.add(left);
-                visited.add(right);
-                
-            }
-            level++;
-        }
-        
-        return res;
-    }
     void buildMap(TreeNode node , TreeNode parent){
         if(node == null)return;
         
@@ -76,4 +19,50 @@ class Solution {
         buildMap(node.right,node);
     }
     
+    
+    public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
+        
+        if (root == null || k < 0) return res;
+         buildMap(root, null);
+        
+         Set <TreeNode> visited = new HashSet();
+        visited.add(null);
+        
+        Queue<TreeNode> q = new LinkedList();
+        q.add(target);
+        int level =0;
+         while(q.size() > 0){
+            
+            if (level == k ){
+                while (q.size()>0){
+                    res.add(q.remove().val);
+                }
+                return res;
+            }
+            
+            int size = q.size();
+            while (size-- > 0){
+                
+                TreeNode temp = q.remove();
+                visited.add(temp);
+                
+//                 addits 3 things 
+                
+                TreeNode parent = map.get(temp);
+                TreeNode left = temp.left;
+                TreeNode right = temp.right;
+                
+                if (! visited.contains(parent))q.add(parent);
+                if (! visited.contains(left))q.add(left);
+                if (! visited.contains(right))q.add(right);
+                
+                visited.add(parent);
+                visited.add(left);
+                visited.add(right);
+                
+            }
+            level++;
+        }
+        return res;
+    }
 }
