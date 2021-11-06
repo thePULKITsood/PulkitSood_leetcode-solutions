cf23bd0924b131f02dffe7b45b22799ed5364ed7
pulkit sood <59098007+thePULKITsood@users.noreply.github.com>
2021-11-06 13:22:38 +0530

    Time: 1 ms (97.61%), Space: 39 MB (91.06%) - LeetHub

---
+++ single-number-iii/single-number-iii.java
@@ -0,0 +1,21 @@
+class Solution {
+  public int[] singleNumber(int[] nums) {
+    // difference between two numbers (x and y) which were seen only once
+    int bitmask = 0;
+    for (int num : nums) bitmask ^= num;
+
+    // rightmost 1-bit diff between x and y
+    int diff = bitmask & (-bitmask);
+
+    int x = 0;
+    // bitmask which will contain only x
+    for (int num : nums) if ((num & diff) != 0) x ^= num;
+
+    return new int[]{x, bitmask^x};
+  }
+}
+/*
+
+-->> Do again Biatch ! 
+
+*/
