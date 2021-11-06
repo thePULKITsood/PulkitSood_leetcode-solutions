387ef947471143e2506d3093414bce2db88b931e
pulkit sood <59098007+thePULKITsood@users.noreply.github.com>
2021-11-06 13:29:08 +0530

    Time: 2 ms (79.72%), Space: 38.9 MB (77.37%) - LeetHub

--- longest-valid-parentheses/longest-valid-parentheses.java
+++ longest-valid-parentheses/longest-valid-parentheses.java
@@ -8,15 +8,29 @@ class Solution {
          
         for (int i = 0; i < n; i++)
         {
-            if (st.peek() != -1 && s.charAt(st.peek()) == '(' && s.charAt(i) == ')')
-            {
-                st.pop();
-                len = Math.max(len, i - st.peek());
-            } 
-            else
-            {
+            
+            if(s.charAt(i)=='('){
                 st.push(i);
             }
+            
+            else {
+                st.pop();
+                if(st.isEmpty()){
+                    st.push(i);
+                }
+                else {
+                    len = Math.max(len,i-st.peek());
+                }
+            }
+            // if (st.peek() != -1 && s.charAt(st.peek()) == '(' && s.charAt(i) == ')')
+            // {
+            //     st.pop();
+            //     len = Math.max(len, i - st.peek());
+            // } 
+            // else
+            // {
+            //     st.push(i);
+            // }
         }
 
         return len;
