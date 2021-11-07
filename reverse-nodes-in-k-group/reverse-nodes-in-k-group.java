bb183fe2c777a801badc3d036680126d8e6526f3
pulkit sood <59098007+thePULKITsood@users.noreply.github.com>
2021-11-08 04:16:13 +0530

    Time: 1 ms (42.30%), Space: 39.4 MB (58.06%) - LeetHub

--- reverse-nodes-in-k-group/reverse-nodes-in-k-group.java
+++ reverse-nodes-in-k-group/reverse-nodes-in-k-group.java
@@ -1,31 +1,37 @@
 
 class Solution {
+    
+    ListNode th = null;
+    ListNode tt = null;
     public ListNode reverseKGroup(ListNode head, int k) {
         if (head==null|| head.next== null || k==0) return head;
         int len = length(head);
         
         ListNode curr = head;
         
-        ListNode oh = null;
-        ListNode ot =null;
+        ListNode oh = null; // original head 
+        ListNode ot =null; // original tail 
+    
         
         while (len>=k)
         {
             int tempk = k ;
+            
             while (tempk-- > 0)
             {
-                ListNode forw = curr.next;
-                curr.next=null;
-                addfirstNode (curr);
-                curr=forw;
+                ListNode forw = curr.next; // basically storing the next node 
+                curr.next=null;            // breaking the link 
+                addfirstNode (curr);       // using the add first to add to curr.next=th   ; th=curr;
+                curr=forw;                 // now do the same to the next node 
             }
-            if (oh==null)
+            if (oh==null)        // now it oh ==null meaning first time 
             {
-                oh =th;
+                oh =th;  // make them equal to the th and tt
                 ot=tt;
                 
             }
-            else 
+            else         // else original tail . next = th 
+                         // and ot==tt 
             {
                 ot.next=th;
                 ot=tt;
@@ -33,6 +39,7 @@ class Solution {
             
                 th=null;
                 tt=null;
+            // at the end after processing nodes 
                 len-=k;
         }
         ot.next=curr;
@@ -40,18 +47,19 @@ class Solution {
     }
     
     
-    ListNode th = null;
-    ListNode tt = null;
-    public void addfirstNode (ListNode node )
+    public void addfirstNode (ListNode node ) // just this function is important and make tt and th global variables                                               // for easy access ! 
     {
-        if (th==null)
+        if (th==null)  // if the first elemnt make the th and tt equal to it 
+            
         {
             th=node;
             tt=node;
         }
         else
-        {node.next=th;
-        th=node;}
+        {
+            node.next=th;
+            th=node;
+        }
     }
     public int length (ListNode node )
     {
