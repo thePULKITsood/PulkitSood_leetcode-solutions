bc6fec92914b8edc14d11ab91acf5c08b39689b2
pulkit sood <59098007+thePULKITsood@users.noreply.github.com>
2021-11-07 23:45:36 +0530

    Time: 1 ms (99.57%), Space: 40.2 MB (44.98%) - LeetHub

--- jump-game-ii/jump-game-ii.java
+++ jump-game-ii/jump-game-ii.java
@@ -1,18 +1,26 @@
 class Solution {
     public int jump(int[] nums) {
-        int jumps = 0;
-        int l =0;
-        int r=0;
+        int min =0;
+        int max =0;
+        int n = nums.length;
+        int num=0;
         
-        while (r < nums.length-1){
-            int farthest = 0;
-            for (int i = l ; i<=r ; i++){
-                farthest = Math.max(farthest,i+nums[i]);
+        while (max<n-1){
+            
+            int newmax= max;
+            
+            //basically the starting point must be between the left ans the right 
+            for (int i=min;i<=max;i++){
+                newmax=Math.max(newmax,i+nums[i]);
+                
             }
-            l=r+1;
-            r=farthest;
-            jumps++;
+            
+            min = max+1; //as wehave scanned all til max so we must go from max +1 now 
+            max=newmax;
+            num++;
+                
         }
-        return jumps;
+        return num;
+        
     }
 }
