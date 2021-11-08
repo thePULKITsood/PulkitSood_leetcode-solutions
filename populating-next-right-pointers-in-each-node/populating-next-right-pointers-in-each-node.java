8664d7116cda3639a7d19c55fc5e52fc06122373
pulkit sood <59098007+thePULKITsood@users.noreply.github.com>
2021-11-08 11:41:22 +0530

    Time: 0 ms (100.00%), Space: 39.2 MB (67.43%) - LeetHub

--- populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.java
+++ populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.java
@@ -21,29 +21,44 @@ class Node {
 };
 */
 
+
+/*
+
+pep coding video jaroori hai ye bohot 
+
+https://www.youtube.com/watch?v=3MRPQFUpoA0
+
+
+We only move on to the level N+1 when we are done establishing the next pointers for the level N. Since we have access to all the nodes on a particular level via the next pointers, we can use these next pointers to establish the connections for the next level or the level containing their children.
+
+
+*/
 class Solution {
     public Node connect(Node root) {
-        if(root==null )return root;
-        Queue<Node> q= new LinkedList();
-        q.add(root);
-        while (!q.isEmpty()){
-            int size =q.size();
-            while (size-->0){
-                Node rem = q.remove();
-                if(size==0 || q.size()==0){
-                    rem.next=null;
-                    
-                }
-                else {
-                    rem.next=q.peek();
-                }
-                if(rem.left!=null)q.add(rem.left);
+
+        if(root==null)return root;
+        
+      Node leftMost = root;
+        
+        while (leftMost.left!=null){
+            Node head = leftMost;
+            
+            while (head != null) {
                 
-                if(rem.right!=null)q.add(rem.right);
+                // CONNECTION 1
+                head.left.next = head.right;
                 
+                // CONNECTION 2
+                if (head.next != null) {
+                    head.right.next = head.next.left;
+                }
+                
+                // Progress along the list (nodes on the current level)
+                head = head.next;
             }
+            
+            leftMost=leftMost.left;
         }
         return root;
-        
     }
 }
