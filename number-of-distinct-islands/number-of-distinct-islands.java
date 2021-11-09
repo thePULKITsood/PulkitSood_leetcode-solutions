74cbc47f9cca2c19f832a130ca6cce2845a4ad8c
pulkit sood <59098007+thePULKITsood@users.noreply.github.com>
2021-11-10 04:26:18 +0530

    Time: 4 ms (99.96%), Space: 39.6 MB (90.08%) - LeetHub

--- number-of-distinct-islands/number-of-distinct-islands.java
+++ number-of-distinct-islands/number-of-distinct-islands.java
@@ -1,44 +1,52 @@
 class Solution {
-    int G[][];
-    boolean visited[][];
+//     int G[][];
+    
+//     boolean visited[][];
+    
     StringBuilder sb ;
     int n,m;
     
     public int numDistinctIslands(int[][] grid) {
-         n=grid.length;
+        n=grid.length;
         m=grid[0].length;
-        this.G=grid;
+        // this.G=grid;
         
-        visited=new boolean[n][m];
+        // visited=new boolean[n][m];
         
         Set<String> set = new HashSet();
         
         for (int i=0;i<n;i++){
             for (int j=0;j<m;j++){
+                if(grid[i][j]==1){
                 
                 sb = new StringBuilder();
-                dfs(i,j,'0');
+                dfs(i,j,'0',grid);
                 
-                if(sb.length()==0) continue;
-                
+                // if(sb.length()==0) continue;
+                    
                 set.add(sb.toString());
+                
+                }
             }
         }
         return set.size();
         
     }
-    void dfs(int i,int j, char c){
+    void dfs(int i,int j, char c,int grid[][]){
         
-        if(i<0 || j< 0 ||i>=n || j>=m )return;
-        if(visited[i][j] || G[i][j]==0)return ;
+        if(i<0 || j< 0 ||i>=n || j>=m || grid[i][j]==0 )return;
         
-        visited[i][j]=true;
+        // if(visited[i][j] || G[i][j]==0)return ;
+        
+        // visited[i][j]=true;
+        grid[i][j]=0;
+        
         sb.append(c);
         
-        dfs(i+1,j,'D');
-        dfs(i-1,j,'U');
-        dfs(i,j+1,'R');
-        dfs(i,j-1,'L');
+        dfs(i+1,j,'D',grid);
+        dfs(i-1,j,'U',grid);
+        dfs(i,j+1,'R',grid);
+        dfs(i,j-1,'L',grid);
         sb.append('0');
     }
 }
