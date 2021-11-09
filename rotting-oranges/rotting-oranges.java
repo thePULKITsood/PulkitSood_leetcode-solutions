78361af54a6b9305fee1fdb5e940c88e188d8273
pulkit sood <59098007+thePULKITsood@users.noreply.github.com>
2021-11-10 03:56:21 +0530

    Time: 3 ms (56.72%), Space: 38.2 MB (90.57%) - LeetHub

--- rotting-oranges/rotting-oranges.java
+++ rotting-oranges/rotting-oranges.java
@@ -1,34 +1,42 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
       
-        LinkedList<Integer> q = new LinkedList<>();
+        Queue<Integer> q = new LinkedList<>();
         
         int fresh=0;
         int n = grid.length;
-        int m = grid[0].length;
+        int m = grid[0].length; // cols 
         
         for (int i = 0 ;i< grid.length; i++){
             for(int j = 0 ; j<grid[0].length ; j++){
+                
                 if (grid[i][j] == 2){
-                    q.addLast(i*m + j);
+                    
+                    q.add(i*m + j); // index = row * col + col 
                 }
                 else if (grid[i][j] == 1 ){
-                    fresh++;
+                    fresh++;  // to check the ending condition 
                 }
             } 
         }
-        if (fresh==0) return 0;
+        
+        if (fresh==0) return 0; // all are rotten 
+        
 //         do the bfs 
         int[][] dir = { {-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+        
         int time = 0;
         
         while (q.size()!=0){
             int size = q.size();
             
             while (size -- > 0 ){
-                int rem = q.removeFirst();
-                int  r = rem /m;
-                int c = rem%m;
+                
+                int rem = q.remove();
+                
+                int  r = rem /m;  // -> row == rem / cols  -> divident   
+                int c = rem%m; //-> col == rem / cols -> remainder 
+                
                 
                 
                 for (int d = 0 ; d< dir.length ; d++){
@@ -36,12 +44,16 @@ class Solution {
                     int x = r + dir[d][0];
                     int y = c + dir[d][1];
                     
-                    if (x >= 0 && y >= 0 &&  x< n &&  y<m  && grid[x][y] == 1){
+                    if (x >= 0 && y >= 0 &&  x< n &&  y<m  && grid[x][y] == 1){ // is valid 
                         
-                        grid[x][y] = 2 ;
-                            fresh--;
+                        grid[x][y] = 2 ;//make it polluted ! 
+                        
+                        
+                        fresh--;
+                        
                         q.add(x *m + y );
-                        if (fresh==0)return time +1;
+                        
+                        if (fresh==0)return time +1; // return time +1 as we started with time as 0 ! 
                     } 
                 }
               
