f1a1a723f7203e587798879bef2dd5be877ad7c8
pulkit sood <59098007+thePULKITsood@users.noreply.github.com>
2021-11-25 09:46:04 +0530

    Time: 1 ms (100.00%), Space: 49.7 MB (34.99%) - LeetHub

--- maximum-subarray/maximum-subarray.java
+++ maximum-subarray/maximum-subarray.java
@@ -1,7 +1,7 @@
 public class Solution {
 public int maxSubArray(int[] A) 
 {
-    int max = Integer.MIN_VALUE, sum = 0;
+    int max = -(int)1e6, sum = 0;
     
     
     for (int i = 0; i < A.length; i++)
