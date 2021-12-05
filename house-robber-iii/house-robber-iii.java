0b61ba8e5beb9322769e7f8e9c780d5fbf054b74
pulkit sood <59098007+thePULKITsood@users.noreply.github.com>
2021-12-05 12:46:24 +0530

    Time: 0 ms (100.00%), Space: 38.6 MB (76.22%) - LeetHub

--- house-robber-iii/house-robber-iii.java
+++ house-robber-iii/house-robber-iii.java
@@ -14,33 +14,33 @@
  * }
  */
 class Solution {
-    HashMap<TreeNode, Integer> robResult = new HashMap<>();
-    HashMap<TreeNode, Integer> notRobResult = new HashMap<>();
-
-    public int helper(TreeNode node, boolean parentRobbed) {
-        if (node == null) {
-            return 0;
-        }
-        if (parentRobbed) {
-            if (robResult.containsKey(node)) {
-                return robResult.get(node);
-            }
-            int result = helper(node.left, false) + helper(node.right, false);
-            robResult.put(node, result);
-            return result;
-        } else {
-            if (notRobResult.containsKey(node)) {
-                return notRobResult.get(node);
-            }
-            int rob = node.val + helper(node.left, true) + helper(node.right, true);
-            int notRob = helper(node.left, false) + helper(node.right, false);
-            int result = Math.max(rob, notRob);
-            notRobResult.put(node, result);
-            return result;
+    class money {
+        int yes = 0 ;
+        int no = 0;
+        
+        money(){};
+        
+        money (int yes , int no ){
+            this.yes = yes;
+            this.no = no;
         }
     }
-
     public int rob(TreeNode root) {
-        return helper(root, false);
+//         include and exclude technique 
+//         with robbery and withour robbery max amount 
+        money ans = rob_(root);
+        return Math.max(ans.yes, ans.no);
     }
+    money rob_(TreeNode root){
+        if (root == null) return new money();
+        
+       money left = rob_(root.left);
+        money right = rob_(root.right);
+        
+        int yesrob = left.no + right.no + root.val;
+        
+        int norob = Math.max(left.yes,left.no) + Math.max(right.yes,right.no);
+        
+        return new money(yesrob,norob);
+    } 
 }
