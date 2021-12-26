94a809a16cc5ea812468a853a1726a5552e35b69
pulkit sood <59098007+thePULKITsood@users.noreply.github.com>
2021-12-26 20:10:05 +0530

    Time: 5 ms (95.46%), Space: 41.6 MB (64.89%) - LeetHub

--- merge-intervals/merge-intervals.java
+++ merge-intervals/merge-intervals.java
@@ -6,13 +6,14 @@ class Solution {
         
         for (int[] i : intervals){
             
-            if (ans.size ()==0 || ans.get(ans.size()-1)[1] < i[0] )
+            if (ans.size ()==0 || ans.get(ans.size()-1)[1] < i[0] ) // 
+                // or the end of the last one is more less than this start
                 ans.add(i);
         
         
             else 
                  ans.get(ans.size()-1) [1] = Math.max(i[1] ,  ans.get(ans.size()-1)[1]);
-               
+               //update its end  ! 
             
             }
         
