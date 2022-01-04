5e54da7059345b3b606a56d8a068d026400e5b23
pulkit sood <59098007+thePULKITsood@users.noreply.github.com>
2022-01-04 10:24:15 +0530

    Time: 1 ms (24.17%), Space: 37.6 MB (5.01%) - LeetHub

---
+++ 476-number-complement/476-number-complement.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int findComplement(int n) {
+         if(n==0)return 1 ;
+        int i =0;
+        int j=0;
+        while (i<n){
+            i+=Math.pow(2,j);
+            j++;
+        }
+        return i^n;
+        
+    }
+}
