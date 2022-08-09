73d543e6f412f33fa7e126b5b269ef1a9a718a64
pulkit sood <59098007+thePULKITsood@users.noreply.github.com>
2022-08-09 07:04:31 +0530

    Time: 35 ms (86.13%), Space: 46.9 MB (58.96%) - LeetHub

--- 823-binary-trees-with-factors/823-binary-trees-with-factors.java
+++ 823-binary-trees-with-factors/823-binary-trees-with-factors.java
@@ -2,6 +2,7 @@ class Solution {
     public int numFactoredBinaryTrees(int[] arr) {
         
         Arrays.sort(arr);
+        
         int N = arr.length;
         int MOD= 1000000007;
         long dp[]= new long[N];
@@ -12,32 +13,27 @@ class Solution {
         
         Map<Integer,Integer> mapOfIndex = new HashMap();
         
-                for (int i = 0 ; i < N ; i++)
-                mapOfIndex.put(arr[i],i);
+         for (int i = 0 ; i < N ; i++)
+            mapOfIndex.put(arr[i],i);
                 
-        for (int i = 0 ; i < N ; i++){
-            
+    
+        for (int i = 0 ; i < N ; i++){    
+           
             int prod = arr[i];
             
-            for (int j = 0 ; j<i;j++){
-                
+            for (int j = 0 ; j<i;j++){    
                 int left = arr[j];
-               
                 if( prod % left == 0){
-                
                     int right = prod/left;
                     // now find right 
                     if(mapOfIndex.containsKey(right)){
-                        dp[i] = (dp[i] + dp[j]*dp[mapOfIndex.get(right)]) % MOD;
                         
+                        dp[i] = (dp[i] + dp[j]  *  dp  [mapOfIndex.get(right) ]  ) % MOD;
                         
+                 
                     }
-                    
-                
                 }
-                
             }
-            
         }
         long ans =0;
         for (long x : dp ) ans +=x;
